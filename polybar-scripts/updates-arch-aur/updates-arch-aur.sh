85cd6d283ab4bce6ee604330898d16abdedba5cb
x70b1 <x70b1@users.noreply.github.com>
2017-12-06 00:27:08 +0100

    just another fix

--- polybar-scripts/updates-arch-aur/updates-arch-aur.sh
+++ polybar-scripts/updates-arch-aur/updates-arch-aur.sh
@@ -1,10 +1,8 @@
  #!/bin/sh
 
-# if ! updates=$(cower -u 2> /dev/null); then
-if ! updates=$(trizen -Su --aur 2> /dev/null); then
+# if ! updates=$(cower -u 2> /dev/null | wc -l); then
+if ! updates=$(trizen -Su --aur 2> /dev/null | wc -l); then
     updates=0
-else
-    updates=$(echo $updates | wc -l)
 fi
 
 if [ "$updates" -gt 0 ]; then
