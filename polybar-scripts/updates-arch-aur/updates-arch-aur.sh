69312363330e67b219836b085fe4e4264d7752da
x70b1 <x70b1@users.noreply.github.com>
2017-12-06 00:02:59 +0100

    pay attention to exit code

--- polybar-scripts/updates-arch-aur/updates-arch-aur.sh
+++ polybar-scripts/updates-arch-aur/updates-arch-aur.sh
@@ -1,7 +1,11 @@
  #!/bin/sh
 
-# updates=$(cower -u | wc -l)
-updates=$(trizen -Su --aur | wc -l)
+# if ! updates=$(cower -u 2> /dev/null); then
+if ! updates=$(trizen -Su --aur 2> /dev/null); then
+    updates=0
+else
+    updates=$(echo $updates | wc -l)
+fi
 
 if [ "$updates" -gt 0 ]; then
     echo "# $updates"
