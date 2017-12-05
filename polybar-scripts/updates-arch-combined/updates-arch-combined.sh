52bd493e73d83b2ccdd3f012292f78a0ea8264ef
x70b1 <x70b1@users.noreply.github.com>
2017-12-06 00:26:35 +0100

    just another fix

--- polybar-scripts/updates-arch-combined/updates-arch-combined.sh
+++ polybar-scripts/updates-arch-combined/updates-arch-combined.sh
@@ -2,10 +2,8 @@
 
 updates_arch=$(checkupdates | wc -l)
 
-if ! updates_aur=$(trizen -Su --aur 2> /dev/null); then
+if ! updates_aur=$(trizen -Su --aur 2> /dev/null | wc -l); then
     updates_aur=0
-else
-    updates_aur=$(echo $updates_aur | wc -l)
 fi
 
 updates=$(("$updates_arch" + "$updates_aur"))
