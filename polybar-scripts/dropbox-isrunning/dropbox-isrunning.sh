e5fff274eaf5d7d647f78fce21e04d22a2df3dac
x70b1 <x70b1@users.noreply.github.com>
2017-12-06 09:58:08 +0100

    rewrite dropbox-isrunning

--- polybar-scripts/dropbox-isrunning/dropbox-isrunning.sh
+++ polybar-scripts/dropbox-isrunning/dropbox-isrunning.sh
@@ -1,36 +1,34 @@
 #!/bin/sh
-# polybar Dropbox module by github.com/ylwghst
-# Feel free to use, share or change it without any restrictions
 
-# These can be changed to hex codes of your favourite colours
-colorRunning="#dfdfdf"
-colorStopped="#555"
+pid=$(pgrep -x "dropbox")
 
-pid=`pgrep -x "dropbox"`
-
-function isRunning {
-  if pgrep -x "dropbox" > /dev/null; then
-    return 0
-  fi
-    return 1
+isRunning() {
+    if pgrep -x "dropbox" > /dev/null; then
+        return 0
+    else
+        return 1
+    fi
 }
 
-if [[ $# -gt 0 ]]; then
-  case "$1" in
-      toggle)
-          if isRunning; then
-              kill $pid; exit 0
-          fi
-          (dropbox &);
-          ;;
-          *)
-              echo 'error: wrong argument'; exit 1
-          ;;
-  esac
+if [ $# -gt 0 ]; then
+    case "$1" in
+        toggle)
+            if isRunning; then
+                kill "$pid";
+                exit 0
+            fi
+            dropbox &
+        ;;
+        *)
+            echo "error: wrong argument";
+            exit 1
+        ;;
+    esac
 fi
 
 if isRunning; then
-    echo "%{F$colorRunning}%{F-}"; exit 0
+    echo "#1"
+    exit 0
 fi
-echo "%{F$colorStopped}%{F-}"
+echo "#2"
 exit 0
