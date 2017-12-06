b330fe1c5b4674708cf71a43e19a527a2b724d06
x70b1 <x70b1@users.noreply.github.com>
2017-12-06 08:09:16 +0100

    rewrite condition

--- polybar-scripts/bluetooth-isactive/bluetooth-isactive.sh
+++ polybar-scripts/bluetooth-isactive/bluetooth-isactive.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-status=$(systemctl is-active bluetooth.service)
-
-if [ "$status" = "active" ]
-then
+if [ "$(systemctl is-active bluetooth.service)" = "active" ]; then
 	echo "#1"
 else
 	echo "#2"
