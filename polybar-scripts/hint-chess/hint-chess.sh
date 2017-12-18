cd449d5b308b6b659f631048859daa93865a8524
x70b1 <x70b1@users.noreply.github.com>
2017-12-18 20:24:13 +0100

    update hint-chess

--- polybar-scripts/hint-chess/hint-chess.sh
+++ polybar-scripts/hint-chess/hint-chess.sh
@@ -2,18 +2,10 @@
 
 # Your chess.com username
 username="your username here"
-
-# Set prefix icon
-icon="# "
-
-# Set prefix icon color
-color_games="#e06c75"
-color_nogames="#e0e0e0"
-
 games=$(curl -s "https://api.chess.com/pub/player/$username/games/to-move" | jq ".[] | length")
 
 if [ "$games" -gt 0 ]; then
-   echo "%{F$color_games}$icon $games"
+   echo "#1 $games"
 else
-   echo "%{F$color_nogames}$icon $games"
+   echo "#2"
 fi
