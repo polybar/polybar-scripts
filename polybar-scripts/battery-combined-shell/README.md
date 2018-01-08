6fd4fb05f8b194f0ceb0f882552b10fab796fea0
x70b1 <x70b1@users.noreply.github.com>
2018-01-08 05:01:57 +0100

    replace battery-combined-python with battery-combined-shell

---
+++ polybar-scripts/battery-combined-shell/README.md
@@ -0,0 +1,15 @@
+# Script: battery-combined-shell
+
+A shell script that shows the battery status.
+
+It supports two rechargeable batteries and changing icons. It works even if only one battery is used.
+
+
+## Module
+
+```
+[module/battery-combined-shell]
+type = custom/script
+exec = ~/polybar-scripts/battery-combined-shell.sh
+interval = 10
+```
---
+++ polybar-scripts/battery-combined-shell/battery-combined-shell.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+path_ac="/sys/class/power_supply/AC"
+path_battery_0="/sys/class/power_supply/BAT0"
+path_battery_1="/sys/class/power_supply/BAT1"
+
+ac=0
+battery_level_0=0
+battery_level_1=0
+battery_max_0=0
+battery_max_1=0
+
+if [ -f "$path_ac/online" ]; then
+    ac=$(cat "$path_ac/online")
+fi
+
+if [ -f "$path_battery_0/energy_now" ]; then
+    battery_level_0=$(cat "$path_battery_0/energy_now")
+fi
+
+if [ -f "$path_battery_0/energy_full" ]; then
+    battery_max_0=$(cat "$path_battery_0/energy_full")
+fi
+
+if [ -f "$path_battery_1/energy_now" ]; then
+    battery_level_1=$(cat "$path_battery_1/energy_now")
+fi
+
+if [ -f "$path_battery_1/energy_full" ]; then
+    battery_max_1=$(cat "$path_battery_1/energy_full")
+fi
+
+battery_level=$(expr $battery_level_0 + $battery_level_1)
+battery_max=$(expr $battery_max_0 + $battery_max_1)
+
+battery_percent=$(expr $battery_level \* 100)
+battery_percent=$(expr $battery_percent / $battery_max)
+
+if [ $ac -eq 1 ]; then
+    icon="#1"
+
+    if [ $battery_percent -gt 97 ]; then
+        echo $icon
+    else
+        echo $icon $battery_percent "%"
+fi
+
+else
+    if [ $battery_percent -gt 85 ]; then
+        icon="#21"
+    elif [ $battery_percent -gt 60 ]; then
+        icon="#22"
+    elif [ $battery_percent -gt 35 ]; then
+        icon="#23"
+    elif [ $battery_percent -gt 10 ]; then
+        icon="#24"
+    else
+        icon="#25"
+    fi
+
+    echo $icon $battery_percent "%"
+fi
