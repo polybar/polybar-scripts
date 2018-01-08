da4cf8621daea207dbf7cb5437bd718a406f399b
x70b1 <x70b1@users.noreply.github.com>
2018-01-08 09:52:13 +0100

    fix inbox-imap-shell

--- polybar-scripts/inbox-imap-shell/inbox-imap-shell.sh
+++ polybar-scripts/inbox-imap-shell/inbox-imap-shell.sh
@@ -4,9 +4,9 @@ SERVER=""
 USER=""
 PASS=""
 
-inbox=$(curl -sf -u "$USER":"$PASS" -X "STATUS INBOX (UNSEEN)" imaps://"$SERVER"/INBOX | cut -d " " -f 5 | tr -d ")")
+inbox=$(curl -sf -u "$USER":"$PASS" -X "STATUS INBOX (UNSEEN)" imaps://"$SERVER"/INBOX | cut -d " " -f 5 | tr -d ")" | tr -d "\r")
 
-if [ $? = 0 ] && [ "$inbox" != 0 ]; then
+if [ $? -eq 0 ] && [ "$inbox" -ne 0 ]; then
     echo "# $inbox"
 else
     echo ""
--- polybar-scripts/inbox-imap-shellnetrc/inbox-imap-shellnetrc.sh
+++ polybar-scripts/inbox-imap-shellnetrc/inbox-imap-shellnetrc.sh
@@ -3,9 +3,9 @@
 SERVER=""
 NETRC=".netrc"
 
-inbox=$(curl -sf --netrc-file "$NETRC" -X "STATUS INBOX (UNSEEN)" imaps://"$SERVER"/INBOX | cut -d " " -f 5 | tr -d ")")
+inbox=$(curl -sf --netrc-file "$NETRC" -X "STATUS INBOX (UNSEEN)" imaps://"$SERVER"/INBOX | cut -d " " -f 5 | tr -d ")" | tr -d "\r")
 
-if [ $? = 0 ] && [ "$inbox" != 0 ]; then
+if [ $? -eq 0 ] && [ "$inbox" -ne 0 ]; then
     echo "# $inbox"
 else
     echo ""
