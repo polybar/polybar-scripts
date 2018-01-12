df8a16670aa7de02089fbae8b1e3a1996ebf458a
x70b1 <x70b1@users.noreply.github.com>
2018-01-12 08:09:13 +0100

    update hint-chess

--- polybar-scripts/hint-chess/hint-chess.sh
+++ polybar-scripts/hint-chess/hint-chess.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# Your chess.com username
-username="your username here"
-games=$(curl -s "https://api.chess.com/pub/player/$username/games/to-move" | jq ".[] | length")
+username=""
+games=$(curl -sf "https://api.chess.com/pub/player/$username/games/to-move" | jq ".[] | length")
 
 if [ "$games" -gt 0 ]; then
    echo "#1 $games"
