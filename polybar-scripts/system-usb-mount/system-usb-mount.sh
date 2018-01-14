b019ef47f8df21a8026d78549d6e509eaf8f7eb0
x70b1 <x70b1@users.noreply.github.com>
2018-01-14 23:00:19 +0100

    update system-usb-mount

--- polybar-scripts/system-usb-mount/system-usb-mount.sh
+++ polybar-scripts/system-usb-mount/system-usb-mount.sh
@@ -4,8 +4,8 @@ devices=$(lsblk -Jplno NAME,TYPE,RM,SIZE,MOUNTPOINT,VENDOR)
 
 case "$1" in
     --mount)
-        for mount in $(echo $devices | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint == null) | .name'); do
-            udisksctl mount --no-user-interaction -b $mount
+        for mount in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint == null) | .name'); do
+            udisksctl mount --no-user-interaction -b "$mount"
 
             # mountpoint=$(udisksctl mount --no-user-interaction -b $mount)
             # mountpoint=$(echo $mountpoint | cut -d " " -f 4 | tr -d ".")
@@ -13,22 +13,22 @@ case "$1" in
         done
         ;;
     --unmount)
-        for unmount in $(echo $devices | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint != null) | .name'); do
-            udisksctl unmount --no-user-interaction -b $unmount
-            udisksctl power-off --no-user-interaction -b $unmount
+        for unmount in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint != null) | .name'); do
+            udisksctl unmount --no-user-interaction -b "$unmount"
+            udisksctl power-off --no-user-interaction -b "$unmount"
         done
         ;;
     *)
         output=""
-        for unmounted in $(echo $devices | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint == null) | .name'); do
-            unmounted=$(echo $unmounted | tr -d "[:digit:]")
-            unmounted=$(echo $devices | jq -r '.blockdevices[]  | select(.name == "'$unmounted'") | .vendor')
-            unmounted=$(echo $unmounted | tr -d ' ')
+        for unmounted in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint == null) | .name'); do
+            unmounted=$(echo "$unmounted" | tr -d "[:digit:]")
+            unmounted=$(echo "$devices" | jq -r '.blockdevices[]  | select(.name == "'"$unmounted"'") | .vendor')
+            unmounted=$(echo "$unmounted" | tr -d ' ')
 
             output="$output# $unmounted   "
         done
 
-        for mounted in $(echo $devices | jq -r '.blockdevices[] | select(.type == "part") | select(.rm == "1") | select(.mountpoint != null) | .size'); do
+        for mounted in $(echo "$devices" | jq -r '.blockdevices[] | select(.type == "part") | select(.rm == "1") | select(.mountpoint != null) | .size'); do
             output="$output# $mounted   "
         done
 
