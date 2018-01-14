9f900e58dfaecc114dce55598a84715ff77ff6ff
x70b1 <x70b1@users.noreply.github.com>
2018-01-14 23:51:38 +0100

    update pulseaudio-simple

--- polybar-scripts/pulseaudio-simple/pulseaudio-simple.sh
+++ polybar-scripts/pulseaudio-simple/pulseaudio-simple.sh
@@ -2,12 +2,12 @@
 
 muted=$(pamixer --sink 0 --get-mute)
 
-if [ "$muted" = true ] ; then
+if [ "$muted" = true ]; then
     echo "#1 --"
 else
     volume=$(pamixer --sink 0 --get-volume)
 
-    if [ "$volume" -gt 49 ] ; then
+    if [ "$volume" -gt 49 ]; then
         echo "#2 $volume %"
     else
         echo "#3 $volume %"
