7e19c5a801f8d6afc6cec16bd84640de569329a4
x70b1 <x70b1@users.noreply.github.com>
2018-01-14 21:40:58 +0100

    update system-cpu-temppercore

--- polybar-scripts/system-cpu-temppercore/system-cpu-temppercore.sh
+++ polybar-scripts/system-cpu-temppercore/system-cpu-temppercore.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sensors | grep Core | awk '{print substr($3, 2, length($3)-5)}' | tr '\n' ' ' | sed 's/ /°C  /g' | sed 's/  $//'
+sensors | grep Core | awk '{print substr($3, 2, length($3)-5)}' | tr "\\n" " " | sed 's/ /°C  /g' | sed 's/  $//'
