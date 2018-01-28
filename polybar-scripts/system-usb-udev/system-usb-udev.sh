a4162f26cae9928fbc2d12916bf4b363470867ca
x70b1 <x70b1@users.noreply.github.com>
2018-01-28 21:56:08 +0100

    update system-usb-udev

--- polybar-scripts/system-usb-udev/system-usb-udev.sh
+++ polybar-scripts/system-usb-udev/system-usb-udev.sh
@@ -3,17 +3,32 @@
 usb_print() {
     devices=$(lsblk -Jplno NAME,TYPE,RM,SIZE,MOUNTPOINT,VENDOR)
     output=""
+    counter=0
 
     for unmounted in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint == null) | .name'); do
         unmounted=$(echo "$unmounted" | tr -d "[:digit:]")
         unmounted=$(echo "$devices" | jq -r '.blockdevices[]  | select(.name == "'"$unmounted"'") | .vendor')
         unmounted=$(echo "$unmounted" | tr -d ' ')
 
-        output="$output#1 $unmounted "
+        if [ $counter -eq 0 ]; then
+            space=""
+        else
+            space="   "
+        fi
+        counter=$((counter + 1))
+
+        output="$output$space#1 $unmounted"
     done
 
     for mounted in $(echo "$devices" | jq -r '.blockdevices[] | select(.type == "part") | select(.rm == "1") | select(.mountpoint != null) | .size'); do
-        output="$output#2 $mounted "
+        if [ $counter -eq 0 ]; then
+            space=""
+        else
+            space="   "
+        fi
+        counter=$((counter + 1))
+
+        output="$output$space#2 $mounted"
     done
 
     echo "$output"
