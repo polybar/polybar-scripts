15d1fb8681ca03fcbcb9f906d512fba15c26d29d
x70b1 <x70b1@users.noreply.github.com>
2018-01-29 09:54:18 +0100

    redshift command call

--- polybar-scripts/info-redshift-temp/info-redshift-temp.sh
+++ polybar-scripts/info-redshift-temp/info-redshift-temp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$(pgrep -x redshift)" ]; then
-    temp=$(redshift -p 2> /dev/null | grep temp | cut -d " " -f 3 | tr -d "[:alpha:]")
+    temp=$(redshift -p 2> /dev/null | grep temp | cut -d ":" -f 2 | tr -dc "[:digit:]")
 
     if [ -z "$temp" ]; then
         echo "%{F#65737E} #"
