f9354c393df99e0b5f14d19b87ae020cc99fd795
x70b1 <x70b1@users.noreply.github.com>
2018-01-31 17:21:04 +0100

    update hint-trash (fix 13)

---
+++ polybar-scripts/info-trash/info-trash.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+case "$1" in
+    --clean)
+        rm -rf ~/.local/share/Trash/files
+        rm -rf ~/.local/share/Trash/info
+        mkdir ~/.local/share/Trash/files
+        mkdir ~/.local/share/Trash/info
+        ;;
+    *)
+        find ~/.local/share/Trash/files/ -maxdepth 1 | wc -l
+        ;;
+esac
