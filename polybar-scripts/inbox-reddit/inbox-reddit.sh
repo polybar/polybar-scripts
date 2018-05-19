c39acf19c674db3c47342d15e87966afe0b7052f
Terencio Agozzino <rememberYou@users.noreply.github.com>
2018-05-19 15:38:05 +0200

    inbox-reddit: fix unread when it is not always an integer (#44)

--- polybar-scripts/inbox-reddit/inbox-reddit.sh
+++ polybar-scripts/inbox-reddit/inbox-reddit.sh
@@ -3,6 +3,11 @@
 url="your url here"
 unread=$(curl -sf "$url" | jq '.["data"]["children"] | length')
 
+case "$unread" in
+    ''|*[!0-9]*)
+	unread=0
+esac;
+
 if [ "$unread" -gt 0 ]; then
    echo "#1 $unread"
 else
