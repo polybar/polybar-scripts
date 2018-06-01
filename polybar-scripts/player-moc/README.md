e98878f10480291f45910f11328a8918bd7121e6
x70b1 <x70b1@users.noreply.github.com>
2018-06-01 21:54:04 +0200

    simplify player-moc

--- polybar-scripts/player-moc/README.md
+++ polybar-scripts/player-moc/README.md
@@ -1,20 +1,15 @@
 # Script: player-moc
 
-A small Script that shows the current song.
+A small script that shows the current song. You can also control mocp.
 
-You can also control mocp.
-
-*Note: You can change the maximum size of the text to be shown in the module by changing the value in "label-maxlen"
 
 ## Module
 
 ```ini
 [module/player-moc]
 type = custom/script
-format = <label>
-label-maxlen = 40
 exec = ~/polybar-scripts/player-moc.sh
-interval = 1
+interval = 3
 click-left = mocp -f
 click-right = mocp -r
 click-middle = mocp -P
--- polybar-scripts/player-moc/player-moc.sh
+++ polybar-scripts/player-moc/player-moc.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
 
-# Change according to information you wish to show in your polybar:
-#        %state     State
-#        %file       File
-#        %title     Title
-#        %artist    Artist
-#        %song      SongTitle
-#        %album     Album
-#        %tt        TotalTime
-#        %tl        TimeLeft
-#        %ts        TotalSec
-#        %ct        CurrentTime
-#        %cs        CurrentSec
-#        %b         Bitrate
-#        %r         Rate
-
-if [ "$(mocp -Q %state)" != "STOP" ]
-then
+if [ "$(mocp -Q %state)" != "STOP" ];then
     echo "$(mocp -Q %song) - $(mocp -Q %album)"
-else 
-    echo ""    
+else
+    echo ""
 fi
