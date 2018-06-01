fe455e54fef249f6baa12a6400a7c25881e809cf
Danilo Freire <39655320+sixatsick@users.noreply.github.com>
2018-06-01 19:04:39 -0300

    Solved shellcheck issues

--- polybar-scripts/player-spotify/player-spotify.sh
+++ polybar-scripts/player-spotify/player-spotify.sh
@@ -5,9 +5,7 @@ main() {
     echo ""; exit
   fi  
 
-  cmd="org.freedesktop.DBus.Properties.Get"
   domain="org.mpris.MediaPlayer2"
-  path="/org/mpris/MediaPlayer2"
 
   meta=$(dbus-send --print-reply --dest=${domain}.spotify \
     /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:${domain}.Player string:Metadata)
@@ -16,7 +14,7 @@ main() {
   album=$(echo "$meta" | sed -nr '/xesam:album"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1| sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
   title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\&/\\&/g'| sed 's#\/#\\/#g')
 
-  echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i | sed "s/\&/\&/g" | sed "s#\/#\/#g"
+  echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i | sed "s#\\&#\\&#g" | sed "s#\\/#\\/#g"
 
 }
 
