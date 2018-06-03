d02318ec513d719e91187836f848cea56a943f38
Davi Alexandre <davi@davialexandre.com.br>
2018-06-03 10:35:27 -0300

    Fix openweathermap scripts for when the API returns more than icon

--- polybar-scripts/openweathermap-detailed/openweathermap-detailed.sh
+++ polybar-scripts/openweathermap-detailed/openweathermap-detailed.sh
@@ -44,9 +44,9 @@ else
 fi
 
 if [ ! -z "$weather" ]; then
-    weather_desc=$(echo "$weather" | jq -r ".weather[].description")
+    weather_desc=$(echo "$weather" | jq -r ".weather[0].description")
     weather_temp=$(echo "$weather" | jq ".main.temp" | cut -d "." -f 1)
-    weather_icon=$(echo "$weather" | jq -r ".weather[].icon")
+    weather_icon=$(echo "$weather" | jq -r ".weather[0].icon")
 
     echo "$(get_icon "$weather_icon")" "$weather_desc", "$weather_temp$SYMBOL"
 fi
