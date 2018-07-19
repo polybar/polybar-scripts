80e7e24f75d53c05af1b67cc5074dc64949b7d75
x70b1 <x70b1@users.noreply.github.com>
2018-07-19 18:26:17 +0200

    pulseaudio-rofi: cleanup

--- polybar-scripts/pulseaudio-rofi/pulseaudio-rofi.sh
+++ polybar-scripts/pulseaudio-rofi/pulseaudio-rofi.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 outputs() {
     OUTPUT=$(pactl list short sinks | cut  -f 2 | rofi -dmenu -p "Output" -mesg "Select prefered output source" )
     pacmd set-default-sink "$OUTPUT" >/dev/null 2>&1
-    for playing in $(pacmd list-sink-inputs | awk '$1 == "index:" {print $2}')
-    do
+
+    for playing in $(pacmd list-sink-inputs | awk '$1 == "index:" {print $2}'); do
         pacmd move-sink-input "$playing" "$OUTPUT" >/dev/null 2>&1
     done
 }
@@ -12,8 +12,8 @@ outputs() {
 inputs() {
     INPUT=$(pactl list short sources | cut  -f 2 | grep input | rofi -dmenu -p "Output" -mesg "Select prefered input source" )
     pacmd set-default-source "$INPUT" >/dev/null 2>&1
-    for recording in $(pacmd list-source-outputs | awk '$1 == "index:" {print $2}')
-    do
+
+    for recording in $(pacmd list-source-outputs | awk '$1 == "index:" {print $2}'); do
         pacmd move-source-output "$recording" "$INPUT" >/dev/null 2>&1
     done
 }
@@ -46,8 +46,7 @@ get_default_sink() {
     pacmd stat | awk -F": " '/^Default sink name: /{print $2}'
 }
 
-output_volume()
-{
+output_volume() {
      pacmd list-sinks | awk '/^\s+name: /{indefault = $2 == "'"<$(get_default_sink)>"'"}
              /^\s+muted: / && indefault {muted=$2}
              /^\s+volume: / && indefault {volume=$5}
@@ -58,30 +57,27 @@ get_default_source() {
     pacmd stat | awk -F": " '/^Default source name: /{print $2}'
 }
 
-input_volume()
-{
+input_volume() {
      pacmd list-sources | awk '/^\s+name: /{indefault = $2 == "'"<$(get_default_source)>"'"}
              /^\s+muted: / && indefault {muted=$2}
              /^\s+volume: / && indefault {volume=$5}
              END { print muted=="no"?volume:"Muted" }'
 }
 
-output_volume_listener()
-{
-   pactl subscribe | while read -r event; do
+output_volume_listener() {
+    pactl subscribe | while read -r event; do
         if echo "$event" | grep -q "change"; then
             output_volume
         fi
-        done 
+    done
 }
 
-input_volume_listener()
-{
-   pactl subscribe | while read -r event; do
+input_volume_listener() {
+    pactl subscribe | while read -r event; do
         if echo "$event" | grep -q "change"; then
             input_volume
         fi
-        done 
+    done
 }
 
 case "$1" in
@@ -103,7 +99,6 @@ case "$1" in
     --volume_down)
         volume_down
     ;;
-
     --volume_source_up)
         volume_source_up
     ;;
@@ -111,10 +106,10 @@ case "$1" in
         volume_source_down
     ;;
     --output_volume)
-        output_volume 
+        output_volume
     ;;
     --input_volume)
-        input_volume 
+        input_volume
     ;;
     --output_volume_listener)
         output_volume
