80a45ed41fd823ce3e2b42fe465ca357dcfc6ce9
Seweryn Niemiec <thugcee@gmail.com>
2018-08-14 22:23:24 +0200

    better script syntax

--- polybar-scripts/system-cpu-cores-temp-pure-sh/cpu_cores_temp.sh
+++ polybar-scripts/system-cpu-cores-temp-pure-sh/cpu_cores_temp.sh
@@ -3,12 +3,12 @@
 WARN_TEMP=50
 WARN_COLOR='#AC3C71'
 
-for t in `cat /sys/devices/platform/coretemp.0/hwmon/hwmon0/temp?_input`; do 
+cat /sys/devices/platform/coretemp.0/hwmon/hwmon0/temp?_input | while read t; do 
 	t=${t%000}
-	if [ $t -gt $WARN_TEMP ]; then
-		echo -n '%{F'$WARN_COLOR'}'$t'˚ '%{F-};
+	if [ "$t" -gt $WARN_TEMP ]; then
+		echo -n '%{F'$WARN_COLOR'}'"$t"'˚ %{F-}';
 	else
-		echo -n $t'˚ ';
+		echo -n "$t˚ ";
 	fi
 done; 
 
