9dfdd61b9c80841e9f0143229fd3e29b4420a87f
Christian Dannie Storgaard <cybolic@gmail.com>
2018-09-21 17:16:54 +0300

    Added comments about yay and pacman

--- polybar-scripts/updates-arch-combined/updates-arch-combined.sh
+++ polybar-scripts/updates-arch-combined/updates-arch-combined.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# If you're using yay, you can uncomment the following line
+# and comment the rest until line 22
+# updates=$(yay -Qu --quiet | wc -l)
+
+# If you do not have `pacman-contrib` installed:
+# if ! updates_arch=$(pacman -Qu --quiet 2> /dev/null | wc -l ); then
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
@@ -10,6 +16,8 @@ if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
 fi
 
 updates=$(("$updates_arch" + "$updates_aur"))
+
+# Comment until here if using yay
 
 if [ "$updates" -gt 0 ]; then
     echo "# $updates"
--- polybar-scripts/updates-arch/updates-arch.sh
+++ polybar-scripts/updates-arch/updates-arch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# if ! updates=$(yay -Qu --repo --quiet 2> /dev/null | wc -l ); then
+# if ! updates=$(pacman -Qu --quiet 2> /dev/null | wc -l ); then
 if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
     updates=0
 fi
