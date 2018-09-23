e64fe017bbc544cea053a35046582b4f9ab4ccf0
x70b1 <x70b1@users.noreply.github.com>
2018-09-23 15:37:44 +0200

    info-pingrtt: made script more stable (close #80)

--- polybar-scripts/info-pingrtt/info-pingrtt.sh
+++ polybar-scripts/info-pingrtt/info-pingrtt.sh
@@ -2,14 +2,18 @@
 
 HOST=127.43.12.64
 
-rtt=$(ping $HOST -c 1 | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
-
-if [ "$rtt" -lt 50 ]; then
-    icon="%{F#3cb703}#%{F-}"
-elif [ "$rtt" -lt 150 ]; then
-    icon="%{F#f9dd04}#%{F-}"
+if ! ping=$(ping -n -c 1 -W 1 $HOST); then
+    echo "# ping failed"
 else
-    icon="%{F#d60606}#%{F-}"
-fi
+    rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
-echo "$icon $rtt ms"
+    if [ "$rtt" -lt 50 ]; then
+        icon="%{F#3cb703}#%{F-}"
+    elif [ "$rtt" -lt 150 ]; then
+        icon="%{F#f9dd04}#%{F-}"
+    else
+        icon="%{F#d60606}#%{F-}"
+    fi
+
+    echo "$icon $rtt ms"
+fi
