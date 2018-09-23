77b7913d41a42fc925d9a6d4b354ae1181275fcb
x70b1 <10016267+x70b1@users.noreply.github.com>
2018-09-23 14:27:56 +0200

    set yay as default

--- polybar-scripts/updates-arch-combined/updates-arch-combined.sh
+++ polybar-scripts/updates-arch-combined/updates-arch-combined.sh
@@ -4,9 +4,9 @@ if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
 
-# if ! updates_aur=$(yay -Qum | wc -l); then
+if ! updates_aur=$(yay -Qum | wc -l); then
 # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
     updates_aur=0
 fi
 
