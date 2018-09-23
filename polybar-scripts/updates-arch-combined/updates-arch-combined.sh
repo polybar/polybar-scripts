fab904211baaf4242d04ad7eac39f87b0d3aee66
x70b1 <10016267+x70b1@users.noreply.github.com>
2018-09-23 14:16:45 +0200

    fix yay statements

--- polybar-scripts/updates-arch-combined/updates-arch-combined.sh
+++ polybar-scripts/updates-arch-combined/updates-arch-combined.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 
-# If you're using yay, you can uncomment the following line
-# and comment the rest until line 22
-# updates=$(yay -Qu --quiet | wc -l)
-
-# If you do not have `pacman-contrib` installed:
-# if ! updates_arch=$(pacman -Qu --quiet 2> /dev/null | wc -l ); then
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
 
+# if ! updates_aur=$(yay -Qum | wc -l); then
 # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
 if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
     updates_aur=0
 fi
 
 updates=$(("$updates_arch" + "$updates_aur"))
-
-# Comment until here if using yay
 
 if [ "$updates" -gt 0 ]; then
     echo "# $updates"
