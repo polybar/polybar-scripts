be4f6a70c579a7cc72f50b690e51549628dc1c0a
Lucas Pugliesi <ferreiraplucas@gmail.com>
2018-10-05 17:39:53 -0300

    updates-arch-aur working with trizen, cower and yay

--- polybar-scripts/updates-arch-aur/updates-arch-aur.sh
+++ polybar-scripts/updates-arch-aur/updates-arch-aur.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-if ! updates=$(yay -Qum | wc -l); then
-# if ! updates=$(cower -u 2> /dev/null | wc -l); then
-# if ! updates=$(trizen -Su --aur --quiet | wc -l); then
-    updates=0
+if type "trizen" >/dev/null 2>&1; then
+  updates=$(trizen -Su --aur --quiet | wc -l)
+elif type "cower" >/dev/null 2>&1; then
+  updates=$(cower -u 2> /dev/null | wc -l)
+elif type "yay" >/dev/null 2>&1; then
+  updates=$(yay -Qum | wc -l)
 fi
 
 if [ "$updates" -gt 0 ]; then
