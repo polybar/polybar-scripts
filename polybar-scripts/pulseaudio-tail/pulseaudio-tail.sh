9d8cacb8525efcb05653443f604c2aac605637f6
Julio B <julio.bacel@gmail.com>
2018-10-14 21:44:13 +0300

    pulseaudio-tail: Add default sink

--- polybar-scripts/pulseaudio-tail/pulseaudio-tail.sh
+++ polybar-scripts/pulseaudio-tail/pulseaudio-tail.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-sink=0
+### Leave this variable unset if you want to use the default sink
+#sink=0
+
+get_default_sink() {
+    sink=$(pacmd list-sinks | sed -n '/\* index:/ s/.*: //p')
+}
 
 volume_up() {
     pactl set-sink-volume $sink +1%
@@ -15,9 +20,10 @@ volume_mute() {
 }
 
 volume_print() {
-    if pacmd list-sinks | grep active | head -n 1 | grep -q speaker; then
+    active_port=$(pacmd list-sinks | sed -n "/index: $sink/,/index:/p" | grep active)
+    if echo "$active_port" | grep -q speaker; then
         icon="#1"
-    elif pacmd list-sinks | grep active | head -n 1 | grep -q headphones; then
+    elif echo "$active_port" | grep -q headphones; then
         icon="#2"
     else
         icon="#3"
@@ -36,12 +42,19 @@ listen() {
     volume_print
 
     pactl subscribe | while read -r event; do
-        if echo "$event" | grep -q "#$sink"; then
+        if echo "$event" | grep -q "sink\ #$sink$"; then
+            volume_print
+        elif [ "$use_default_sink" ] && echo "$event" | grep -q "'change'\ on\ server"; then
+            get_default_sink
             volume_print
         fi
     done
 }
 
+if [ -z "$sink" ]; then
+    use_default_sink=true
+    get_default_sink
+fi
 case "$1" in
     --up)
         volume_up
