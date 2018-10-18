930c4eca956b86b35ff06046c2677a234850b29f
Christer Jensen <chr.code@gmail.com>
2018-10-19 00:20:04 +0200

    Make player-moc use filename if no song name
    Not every file necessarily has the song field set to anything. In those
    cases to avoid empty output, e.i. " - ", the filename is printed instead.

--- polybar-scripts/player-moc/player-moc.sh
+++ polybar-scripts/player-moc/player-moc.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 if [ "$(mocp -Q %state)" != "STOP" ];then
-    echo "$(mocp -Q %song) - $(mocp -Q %album)"
+    SONG=$(mocp -Q %song)
+    if [ -z "$SONG" ]; then
+        basename "$(mocp -Q %file)"
+    else
+        echo "$SONG - $(mocp -Q %album)"
+    fi
 else
     echo ""
 fi
