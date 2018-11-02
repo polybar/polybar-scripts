5df197b72335a97c1cf6fb42e6954eb301640a52
shoo <shuzzd@gmail.com>
2018-11-02 22:39:47 +0000

    fixed shellchek issues

--- polybar-scripts/player-cmus/player-cmus.sh
+++ polybar-scripts/player-cmus/player-cmus.sh
@@ -2,7 +2,7 @@
 
 status=$(cmus-remote -Q | grep status | awk -F\  '{print $2}')
 title=$(cmus-remote -Q | grep title | awk -F\  '{$1=$2=""; print $0}')
-artist=$(cmus-remote -Q | grep [[:space:]]artist | awk -F\  '{$1=$2=""; print $0}')
+artist=$(cmus-remote -Q | grep '[[:space:]]artist' | awk -F\  '{$1=$2=""; print $0}')
 position=$(cmus-remote -Q | grep position | awk -F\  '{print $2}') 
 duration=$(cmus-remote -Q | grep duration | awk -F\  '{print $2}')
 
