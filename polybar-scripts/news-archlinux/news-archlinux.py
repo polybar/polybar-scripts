c699efd229b200bb33fe5beb66f8cfa695849748
Radim Keske <radim.keske@gmail.com>
2018-11-14 20:08:42 +0100

    removed unnecessary lines

--- polybar-scripts/news-archlinux/news-archlinux.py
+++ polybar-scripts/news-archlinux/news-archlinux.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python
-
 import feedparser
-from subprocess import call
-import re
-import textwrap
 
 d = feedparser.parse("https://www.archlinux.org/feeds/news/")
-
-for f in range(0, 1):
-    print(d.entries[f].title)
-    xy = d.entries[f].title
+print(d.entries[0].title)
+print(d.entries[1].title)
