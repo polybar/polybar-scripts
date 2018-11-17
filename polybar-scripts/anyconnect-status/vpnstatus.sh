4eb62fb3feb4ee5263ab9fd1921a797fa74a1815
Kevin Mancuso <kevin@mancu.so>
2018-11-17 00:33:05 -0600

    Make CI happy

--- polybar-scripts/anyconnect-status/vpnstatus.sh
+++ polybar-scripts/anyconnect-status/vpnstatus.sh
@@ -4,7 +4,7 @@ IFACE=$(ifconfig | grep cscotun | awk '{print $1}')
 
 if [[ $IFACE == "cscotun"* ]];
 then
-    echo "$(ifconfig cscotun0 | grep inet | awk '{print $2}' | cut -f2 -d':')"
+    ifconfig cscotun0 | grep inet | awk '{print $2}' | cut -f2 -d':'
 else
     echo 'VPN DISCONNECTED'
 fi
