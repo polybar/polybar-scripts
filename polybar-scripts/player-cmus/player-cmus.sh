33ace3e9b43e41a72383329a067b89568c941ae6
shoo <shuzzd@gmail.com>
2018-11-22 00:11:13 +0000

    fixed shellcheck issues, fixed cmus-remote parsing (removed leading spaces in artist and title)

--- polybar-scripts/player-cmus/player-cmus.sh
+++ polybar-scripts/player-cmus/player-cmus.sh
@@ -2,25 +2,25 @@
 # shellcheck disable=SC2034
 
 status=$(cmus-remote -Q | grep status | awk -F\  '{print $2}')
-title=$(cmus-remote -Q | grep title | awk -F\  '{$1=$2=""; print $0}')
-artist=$(cmus-remote -Q | grep '[[:space:]]artist' | awk -F\  '{$1=$2=""; print $0}')
+title=$(cmus-remote -Q | grep title | awk -F\  '{$1=$2=""; print $0}' | sed 's|^[[:blank:]]*||g')
+artist=$(cmus-remote -Q | grep '[[:space:]]artist' | awk -F\  '{$1=$2=""; print $0}' | sed 's|^[[:blank:]]*||g')
 position=$(cmus-remote -Q | grep position | awk -F\  '{print $2}') 
 duration=$(cmus-remote -Q | grep duration | awk -F\  '{print $2}')
 
-(( 'pos_minutes = position / 60' ))
+pos_minutes=$(("$position" / 60))
 pos_minutes=$(printf "%02d" "$pos_minutes") 
-(( 'pos_seconds = position % 60' ))
+pos_seconds=$(("$position" % 60))
 pos_seconds=$(printf "%02d" "$pos_seconds") 
 
-(( 'dur_minutes = duration / 60' ))
+dur_minutes=$(("$duration" / 60))
 dur_minutes=$(printf "%02d" "$dur_minutes") 
-(( 'dur_seconds = duration % 60' ))
+dur_seconds=$(("$duration" % 60))
 dur_seconds=$(printf "%02d" "$dur_seconds") 
 
-if [ "$status" == 'stopped' ]
+if test "$status" = 'stopped';
 then
 	echo "$status"
-elif [ "$status" == 'playing' ] || [ "$status" == 'paused' ] 
+elif test "$status" = 'playing' || "$status" = 'paused'; 
 then
 	echo "$status" \| "$artist" - "$title" \| "$pos_minutes":"$pos_seconds" / "$dur_minutes":"$dur_seconds"
 fi
