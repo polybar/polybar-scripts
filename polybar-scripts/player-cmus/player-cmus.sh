fed629ed1ec18ec122a6ff3a4bed7aaaa9c07709
x70b1 <x70b1@users.noreply.github.com>
2018-11-22 15:00:27 +0100

    player-cmus: rewrite script logic

--- polybar-scripts/player-cmus/player-cmus.sh
+++ polybar-scripts/player-cmus/player-cmus.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 
-if [ "$(cmus-remote -Q)" ]; then
-	status=$(cmus-remote -Q | grep status | awk -F\  '{print $2}')
-	title=$(cmus-remote -Q | grep title | awk -F\  '{$1=$2=""; print $0}' | sed 's|^[[:blank:]]*||g')
-	artist=$(cmus-remote -Q | grep '[[:space:]]artist' | awk -F\  '{$1=$2=""; print $0}' | sed 's|^[[:blank:]]*||g')
-	position=$(cmus-remote -Q | grep position | awk -F\  '{print $2}')
-	duration=$(cmus-remote -Q | grep duration | awk -F\  '{print $2}')
+if info=$(cmus-remote -Q 2> /dev/null); then
+	status=$(echo "$info" | grep status | awk -F\  '{print $2}')
 
-	pos_minutes=$(("$position" / 60))
-	pos_minutes=$(printf "%02d" "$pos_minutes")
-	pos_seconds=$(("$position" % 60))
-	pos_seconds=$(printf "%02d" "$pos_seconds")
+	if [ "$status" = "playing" ] || [ "$status" = "paused" ]; then
+		title=$(echo "$info" | grep title | awk -F\  '{$1=$2=""; print $0}' | sed 's|^[[:blank:]]*||g')
+		artist=$(echo "$info" | grep '[[:space:]]artist' | awk -F\  '{$1=$2=""; print $0}' | sed 's|^[[:blank:]]*||g')
+		position=$(echo "$info" | grep position | awk -F\  '{print $2}')
+		duration=$(echo "$info" | grep duration | awk -F\  '{print $2}')
 
-	dur_minutes=$(("$duration" / 60))
-	dur_minutes=$(printf "%02d" "$dur_minutes")
-	dur_seconds=$(("$duration" % 60))
-	dur_seconds=$(printf "%02d" "$dur_seconds")
+		pos_minutes=$(printf "%02d" $(("$position" / 60)))
+		pos_seconds=$(printf "%02d" $(("$position" % 60)))
 
-	if [ "$status" = "stopped" ]; then
-		echo "$status"
-	elif [ "$status" = "playing" ] || [ "$status" = "paused" ]; then
-		echo "$status" \| "$artist" - "$title" \| "$pos_minutes":"$pos_seconds" / "$dur_minutes":"$dur_seconds"
+		dur_minutes=$(printf "%02d" $(("$duration" / 60)))
+		dur_seconds=$(printf "%02d" $(("$duration" % 60)))
+
+		if [ "$status" = "playing" ]; then
+			echo "#1 $artist - $title | $pos_minutes:$pos_seconds / $dur_minutes:$dur_seconds"
+		elif [ "$status" = "paused" ]; then
+			echo "#2 $artist - $title | $pos_minutes:$pos_seconds / $dur_minutes:$dur_seconds"
+		else
+			echo ""
+		fi
+	else
+		echo ""
 	fi
+else
+	echo ""
 fi
