ecfbaa45400d6184f73dd447313925f92c74828c
x70b1 <x70b1@users.noreply.github.com>
2018-11-22 10:43:01 +0100

    small changes

--- polybar-scripts/player-cmus/player-cmus.sh
+++ polybar-scripts/player-cmus/player-cmus.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
-# shellcheck disable=SC2034
 
 status=$(cmus-remote -Q | grep status | awk -F\  '{print $2}')
 title=$(cmus-remote -Q | grep title | awk -F\  '{$1=$2=""; print $0}' | sed 's|^[[:blank:]]*||g')
 artist=$(cmus-remote -Q | grep '[[:space:]]artist' | awk -F\  '{$1=$2=""; print $0}' | sed 's|^[[:blank:]]*||g')
-position=$(cmus-remote -Q | grep position | awk -F\  '{print $2}') 
+position=$(cmus-remote -Q | grep position | awk -F\  '{print $2}')
 duration=$(cmus-remote -Q | grep duration | awk -F\  '{print $2}')
 
 pos_minutes=$(("$position" / 60))
-pos_minutes=$(printf "%02d" "$pos_minutes") 
+pos_minutes=$(printf "%02d" "$pos_minutes")
 pos_seconds=$(("$position" % 60))
-pos_seconds=$(printf "%02d" "$pos_seconds") 
+pos_seconds=$(printf "%02d" "$pos_seconds")
 
 dur_minutes=$(("$duration" / 60))
-dur_minutes=$(printf "%02d" "$dur_minutes") 
+dur_minutes=$(printf "%02d" "$dur_minutes")
 dur_seconds=$(("$duration" % 60))
-dur_seconds=$(printf "%02d" "$dur_seconds") 
+dur_seconds=$(printf "%02d" "$dur_seconds")
 
-if test "$status" = 'stopped';
-then
+if [ "$status" = "stopped" ];then
 	echo "$status"
-elif test "$status" = 'playing' || "$status" = 'paused'; 
-then
+elif [ "$status" = "playing" ] || [ "$status" = "paused" ];then
 	echo "$status" \| "$artist" - "$title" \| "$pos_minutes":"$pos_seconds" / "$dur_minutes":"$dur_seconds"
 fi
