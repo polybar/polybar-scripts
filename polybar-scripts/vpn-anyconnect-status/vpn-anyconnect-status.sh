c0a13cc66f2c425607278b990ee7e91e641b3ef4
x70b1 <x70b1@users.noreply.github.com>
2018-11-23 09:55:27 +0100

    vpn-anyconnect-status: change colored output to default

--- polybar-scripts/vpn-anyconnect-status/vpn-anyconnect-status.sh
+++ polybar-scripts/vpn-anyconnect-status/vpn-anyconnect-status.sh
@@ -2,14 +2,8 @@
 
 IFACE=$(ifconfig | grep cscotun | awk '{print $1}')
 
-if [ "$IFACE" = "cscotun*" ];
-then
-    # Uncomment the following if you want colorized outputs
-    # echo "#1 %{u#55aa55}$(ifconfig cscotun0 | grep inet | awk '{print $2}' | cut -f2 -d':')%{u-}"
-    echo "#1 $(ifconfig cscotun0 | grep inet | awk '{print $2}' | cut -f2 -d':')"
+if [ "$IFACE" = "cscotun*" ]; then
+    echo "#1 %{u#55aa55}$(ifconfig cscotun0 | grep inet | awk '{print $2}' | cut -f2 -d ':')%{u-}"
 else
-    # Uncomment the following if you want colorized outputs
-    # echo "#1 %{F#FF0000}%{u#FF0000}VPN DISCONNECTED%{u-}%{F-}""
-    echo "#1 VPN DISCONNECTED"
+    echo "#1 %{F#FF0000}%{u#FF0000}VPN DISCONNECTED%{u-}%{F-}"
 fi
-
