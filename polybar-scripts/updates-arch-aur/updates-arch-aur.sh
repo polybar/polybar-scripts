266eccaa2924e0dcddcb1158df2eecda95d9eb38
x70b1 <x70b1@users.noreply.github.com>
2018-12-06 13:07:23 +0100

    updates-arch-aur,updates-arch-combined: improve yay, fix (#81)

--- polybar-scripts/updates-arch-aur/updates-arch-aur.sh
+++ polybar-scripts/updates-arch-aur/updates-arch-aur.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if ! updates=$(yay -Qum | wc -l); then
+if ! updates=$(yay -Qum 2> /dev/null | wc -l); then
 # if ! updates=$(cower -u 2> /dev/null | wc -l); then
 # if ! updates=$(trizen -Su --aur --quiet | wc -l); then
     updates=0
--- polybar-scripts/updates-arch-combined/updates-arch-combined.sh
+++ polybar-scripts/updates-arch-combined/updates-arch-combined.sh
@@ -4,7 +4,7 @@ if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
 
-if ! updates_aur=$(yay -Qum | wc -l); then
+if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
 # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
 # if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
     updates_aur=0
