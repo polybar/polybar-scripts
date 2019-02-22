085dbf77a21a41c2bf281747606b94791bc9c27f
Sid Reed <sidreed34@gmail.com>
2019-02-22 13:21:31 -0500

    Travis CI compatibility changes

--- polybar-scripts/info-t-tasklist-summary/task_counter.sh
+++ polybar-scripts/info-t-tasklist-summary/task_counter.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 tskfile="$HOME/tasks/tasks"
-task_cats=$(cat "$tskfile" | cut -d':' -f1 | sort | uniq -c | sort -n)
-total=$(cat "$tskfile" | wc -l)
+task_cats=$(cut -d':' -f1 "$tskfile"| sort | uniq -c | sort -n)
+total=$(wc -l "$tskfile")
 outp="Tasks:$total|"
 while read -r catg; do
-    catg=`echo "$catg" | cut -d' ' -f2`
-    count=`grep "^$catg" "$tskfile" | wc -l | cut -d' ' -f2`
+    catg=$(echo "$catg" | cut -d' ' -f2)
+    count=$(grep "^$catg" "$tskfile" | wc -l | cut -d' ' -f2)
     outp+="$catg:$count|"
 done <<< "$task_cats"
 echo "${outp::-1}|"
