bf9778335e5ebec54cacda2e2b92aa532aa9ff05
Sid Reed <sidreed34@gmail.com>
2019-02-22 13:26:26 -0500

    changed grep | wc -l to grep -c

--- polybar-scripts/info-t-tasklist-summary/task_counter.sh
+++ polybar-scripts/info-t-tasklist-summary/task_counter.sh
@@ -6,7 +6,7 @@ total=$(wc -l "$tskfile")
 outp="Tasks:$total|"
 while read -r catg; do
     catg=$(echo "$catg" | cut -d' ' -f2)
-    count=$(grep "^$catg" "$tskfile" | wc -l | cut -d' ' -f2)
+    count=$(grep -c "^$catg" "$tskfile" | cut -d' ' -f2)
     outp+="$catg:$count|"
 done <<< "$task_cats"
 echo "${outp::-1}|"
