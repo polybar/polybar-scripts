64ff247b595f9ec16ba0c4c1f742f1f98bae658c
jpvolt <jpvoltdeveloper@gmail.com>
2019-03-28 08:36:15 -0300

    Added suport to pikaur aur manager on updates-arch-aur.sh and updates-arch-combined.sh

--- polybar-scripts/updates-arch-aur/updates-arch-aur.sh
+++ polybar-scripts/updates-arch-aur/updates-arch-aur.sh
@@ -3,6 +3,7 @@
 if ! updates=$(yay -Qum 2> /dev/null | wc -l); then
 # if ! updates=$(cower -u 2> /dev/null | wc -l); then
 # if ! updates=$(trizen -Su --aur --quiet | wc -l); then
+# if ! updates=$(pikaur -Qua 2> /dev/null | wc -l); then
     updates=0
 fi
 
