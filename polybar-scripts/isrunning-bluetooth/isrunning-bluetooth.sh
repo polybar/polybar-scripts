b67e61c3dee0b2d25a80a781a2a65164ec00db0a
x70b1 <x70b1@users.noreply.github.com>
2019-08-04 13:25:49 +0200

    isrunning-docker,isrunning-bluetooth: remove unneeded scripts

--- polybar-scripts/isrunning-bluetooth/isrunning-bluetooth.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "$(systemctl is-active bluetooth.service)" = "active" ]; then
-	echo "#1"
-else
-	echo "#2"
-fi
--- polybar-scripts/isrunning-docker/isrunning-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "$(systemctl is-active docker.service)" = "active" ]; then
-    echo "#1"
-else
-    echo "#2"
-fi
