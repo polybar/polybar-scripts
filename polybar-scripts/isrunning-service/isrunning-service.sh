31b58e72eaf705d3588b33a1a87e6ec05af725cd
x70b1 <x70b1@users.noreply.github.com>
2019-08-04 13:22:17 +0200

    isrunning-service: made script more simple

--- polybar-scripts/isrunning-service/isrunning-service.sh
+++ polybar-scripts/isrunning-service/isrunning-service.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-print_status() {
-    if [ "$(systemctl is-active "$UNIT")" = "active" ]; then
+UNIT="docker.service"
+
+journalctl --follow -o cat --unit $UNIT | while read -r; do
+	if [ "$(systemctl is-active "$UNIT")" = "active" ]; then
         echo "#1"
     else
         echo "#2"
     fi
-}
-
-UNIT="bluetooth.service"
-
-
-print_status
-
-journalctl --follow -o cat --since now --unit $UNIT | while read -r; do
-    print_status
 done
