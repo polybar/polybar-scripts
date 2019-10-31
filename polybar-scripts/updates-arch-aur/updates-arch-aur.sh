3e90dddaba804af80d13a00ca918c9c071dea8fd
Oscar Ablinger <dev.ablinger@gmail.com>
2019-10-31 16:41:04 +0100

    updates-arch-aur,updates-arch-combined: add rua support

--- polybar-scripts/updates-arch-aur/updates-arch-aur.sh
+++ polybar-scripts/updates-arch-aur/updates-arch-aur.sh
@@ -4,6 +4,7 @@ if ! updates=$(yay -Qum 2> /dev/null | wc -l); then
 # if ! updates=$(cower -u 2> /dev/null | wc -l); then
 # if ! updates=$(trizen -Su --aur --quiet | wc -l); then
 # if ! updates=$(pikaur -Qua 2> /dev/null | wc -l); then
+# if ! updates=$(rua upgrade --printonly | wc -l); then
     updates=0
 fi
 
