822ff2b288a17947fdcc6bf7f15e3bb926eded3c
Oscar Ablinger <dev.ablinger@gmail.com>
2019-11-03 16:18:58 +0100

    updates-arch-combined: fix rua error outputs

--- polybar-scripts/updates-arch-combined/updates-arch-combined.sh
+++ polybar-scripts/updates-arch-combined/updates-arch-combined.sh
@@ -8,7 +8,7 @@ if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
 # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
 # if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
 # if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
-# if ! updates_aur=$(rua upgrade --printonly | wc -l); then
+# if ! updates_aur=$(rua upgrade --printonly 2> /dev/null | wc -l); then
     updates_aur=0
 fi
 
