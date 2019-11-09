054873429d2f12ac7fd5af2ffca730b06d7edd99
flappy <d3rrial@gmail.com>
2019-11-09 19:05:53 +0100

    followed some travis suggestions

--- polybar-scripts/system-cpu-avg-freq/system-cpu-avg-freq.sh
+++ polybar-scripts/system-cpu-avg-freq/system-cpu-avg-freq.sh
@@ -3,9 +3,9 @@
 c=0;
 t=0;
 
-for i in $( cat /proc/cpuinfo | awk '/MHz/ {print $4}' )
+for i in $( awk '/MHz/ {print $4}' < /proc/cpuinfo )
 do
-    t=$( echo $t+$i | bc )
+    t=$( echo "$t + $i" | bc )
     ((c++))
 done
 echo "scale=2; $t / $c / 1000" | bc | awk '{print $1" GHz"}'
