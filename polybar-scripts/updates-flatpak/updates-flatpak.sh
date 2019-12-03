6a740295bf7b32683e8bf1e2bb7c8d0cfac199be
J2ghz <j2.00ghz@gmail.com>
2019-12-03 21:22:01 +0100

    fix updates-flatpak spellcheck warning

--- polybar-scripts/updates-flatpak/updates-flatpak.sh
+++ polybar-scripts/updates-flatpak/updates-flatpak.sh
@@ -3,7 +3,9 @@
 if ! updates=$(echo "n" | flatpak update 2> /dev/null | wc -l ); then
     updates=0
 fi
-updates=$(($updates - 5))
+
+updates=$((updates - 5))
+
 if [ "$updates" -gt 0 ]; then
     echo "flatpak: $updates"
 else
