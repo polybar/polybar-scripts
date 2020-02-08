9585cdeb8d7bb72e505602dad1d5317e53aca91b
Kim Brose <kim.brose@rwth-aachen.de>
2020-02-08 14:50:27 +0100

    Make vpn-wireguard-wg.sh more flexible
    - wireguard interface is chosen via command line argument
    - give some tips on how to use and customize

--- polybar-scripts/vpn-wireguard-wg/vpn-wireguard-wg.sh
+++ polybar-scripts/vpn-wireguard-wg/vpn-wireguard-wg.sh
@@ -2,9 +2,9 @@
 
 connection_status() {
     if [ -f "$config" ]; then
-        connection=$(sudo wg show "$config_name" 2>/dev/null | head -n 1 | awk '{print $NF }')
+        connection=$(sudo wg show "$name" 2>/dev/null | head -n 1 | awk '{print $NF }')
 
-        if [ "$connection" = "$config_name" ]; then
+        if [ "$connection" = "$name" ]; then
             echo "1"
         else
             echo "2"
@@ -14,10 +14,13 @@ connection_status() {
     fi
 }
 
-config="$HOME/wg/wireguard.conf"
-config_name=$(basename "${config%.*}")
+name="$1"
+# where do you keep your wg config files? make sure you have read access (without sudo)
+# if your configs are installed to /etc/wireguard, you need to chmod 755 /etc/wireguard,
+# use $name instead of $config in the toggle block, and edit the following path
+config="$HOME/wg/$name.conf"
 
-case "$1" in
+case "$2" in
 --toggle)
     if [ "$(connection_status)" = "1" ]; then
         sudo wg-quick down "$config" 2>/dev/null
@@ -27,11 +30,17 @@ case "$1" in
     ;;
 *)
     if [ "$(connection_status)" = "1" ]; then
-        echo "#1 $config_name"
+        echo "#1 $name"
+        # alternatively use below commands to print VPN's IP/subnet
+        # vpn_ip=$(ip a show $name primary | grep "inet" | awk -v OFS="\n" '{ print $2 }')
+        # echo $vpn_ip
     elif [ "$(connection_status)" = "3" ]; then
         echo "#3 Config not found!"
     else
         echo "#2 down"
+        # alternatively use a symbol:
+        # color="#f90000"
+        # echo "%{T2}%{F$color}%{F-T-}"
     fi
     ;;
 esac
