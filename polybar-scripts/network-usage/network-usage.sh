4808b61720edc661973d10bff1de3ec5979f3a33
keystroke3 <keystroke33@gmail.com>
2020-02-20 10:59:12 +0300

    fixed errors with no connection

--- polybar-scripts/network-usage/network-usage.sh
+++ polybar-scripts/network-usage/network-usage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-units() {
+format() {
     if [ "$1" -eq 0 ] || [ "$1" -lt 1000 ]; then
-        bytes="0 kB/s"
+        bytes="off"
     elif [ "$1" -lt 1000000 ]; then
         bytes="$(echo "scale=0;$1/1000" | bc -l ) KB"
     else
@@ -12,16 +12,22 @@ units() {
     echo "$bytes"
 }
 
-interface=$(ip route | grep -oPm1 "(?<=dev )[^ ]+")
+interface=`ip route | grep -oPm1 "(?<=dev )[^ ]+"`
 declare -A bytes
-
-down=$(cat /sys/class/net/"$interface"/statistics/rx_bytes)
-up=$(cat /sys/class/net/"$interface"/statistics/tx_bytes)
+down_file=/sys/class/net/"$interface"/statistics/rx_bytes
+up_file=/sys/class/net/"$interface"/statistics/tx_bytes
+if [ -f $down_file ];then
+    down=$(cat $down_file)
+    up=$(cat $up_file)
+else
+    down=0
+    up=0
+fi
 
 case $1 in
     total)
-        echo "$(units $(( up+down )))" ;;
+        echo $(format $(( up+down ))) ;;
     split)
-        echo "$(units $down)/$(units $up)" ;;
+        echo $(format $down)/$(format $up) ;;
 esac
 
