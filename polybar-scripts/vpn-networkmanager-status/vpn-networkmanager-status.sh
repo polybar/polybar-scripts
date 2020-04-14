0424be6f4c11a31cb56044239f52ab4fac7365bb
x70b1 <x70b1@users.noreply.github.com>
2020-04-14 15:53:33 +0200

    vpn-networkmanager-status: update script

--- polybar-scripts/vpn-networkmanager-status/vpn-networkmanager-status.sh
+++ polybar-scripts/vpn-networkmanager-status/vpn-networkmanager-status.sh
@@ -1,18 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-# Define Status Messages
-disconnectedMessage="#1 VPN: DOWN"
-connectedMessagePreamble="#2 VPN:"
+vpn="$(nmcli -t -f name,type connection show --order name --active 2>/dev/null | grep vpn | head -1 | cut -d ':' -f 1)"
 
-# Acquire raw output of status
-connectionName="$(nmcli c show --active | grep -Eo '^[^ ]+' | grep -v NAME | head -1)"
-active="$(nmcli c show --active | grep tun | head -1)"
-# Check to see if the VPN is active; done by checking whether "tun" is an active interface
-if [ -n "$active" ]
-then
-	connectedMessage="$connectedMessagePreamble $connectionName"
-	echo "$connectedMessage"
+if [ -n "$vpn" ]; then
+	echo "#2 VPN: $vpn"
 else
-	echo "$disconnectedMessage"
+	echo "#2 VPN: DOWN "
 fi
-
