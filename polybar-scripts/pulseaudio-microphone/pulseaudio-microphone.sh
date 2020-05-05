9630c5db4d3d2a349d76d70df8008b2025a7b00f
x70b1 <x70b1@users.noreply.github.com>
2020-05-05 13:39:11 +0200

    improve script

--- polybar-scripts/pulseaudio-microphone/pulseaudio-microphone.sh
+++ polybar-scripts/pulseaudio-microphone/pulseaudio-microphone.sh
@@ -1,50 +1,41 @@
 #!/bin/sh
 
-#
-# Prints the default microphone status (muted or unmuted). Uses PulseAudio.
-#
-
-# Global variables
-DISABLED_COLOR="#777" # Color when the mic is off.
-ENABLED_INDICATOR="#1" # Indicator when the mic is on.
-DISABLED_INDICATOR="#2" # Indicator when the mic is off.
-
-check_status() {
+status() {
   MUTED=$(pacmd list-sources | awk '/*/,EOF {print}' | awk '/muted/ {print $2; exit}')
 
   if [ "$MUTED" = "yes" ]; then
-    echo "%{F$DISABLED_COLOR}$DISABLED_INDICATOR"
+    echo "#1"
   else
-    echo "$ENABLED_INDICATOR"
+    echo "#2"
   fi
 }
 
 listen() {
-  pactl subscribe | while read -r event; do
-    if echo "$event" | grep -q "source" || echo "$event" | grep -q "server"; then
-      check_status
-    fi
-  done
+    status
+
+    LANG=EN; pactl subscribe | while read -r event; do
+        if echo "$event" | grep -q "source" || echo "$event" | grep -q "server"; then
+            status
+        fi
+    done
 }
 
-toggle_status() {
+toggle() {
   MUTED=$(pacmd list-sources | awk '/*/,EOF {print}' | awk '/muted/ {print $2; exit}')
   DEFAULT_SOURCE=$(pacmd list-sources | awk '/*/,EOF {print $3; exit}')
 
   if [ "$MUTED" = "yes" ]; then
-    pacmd set-source-mute "$DEFAULT_SOURCE" 0
+      pacmd set-source-mute "$DEFAULT_SOURCE" 0
   else
-    pacmd set-source-mute "$DEFAULT_SOURCE" 1
+      pacmd set-source-mute "$DEFAULT_SOURCE" 1
   fi
 }
 
 case "$1" in
-  --toggle)
-    toggle_status
-    ;;
-  *)
-    check_status
-    listen
-    ;;
+    --toggle)
+        toggle
+        ;;
+    *)
+        listen
+        ;;
 esac
-
