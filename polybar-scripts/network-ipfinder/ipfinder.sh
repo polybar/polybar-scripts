21d9e29733aa84bc07398585ddc77d7841fe5233
Montazar Al-Jaber <26507935+muntjaber@users.noreply.github.com>
2020-06-09 14:27:50 +0200

    polybar-ipfinder: remove bash specific features

--- polybar-scripts/network-ipfinder/ipfinder.sh
+++ polybar-scripts/network-ipfinder/ipfinder.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Sometimes, when there is no Internet connection, curl will continue to retry. If the total time is longer than MAX_TIME we want to abort. 
-declare -r MAX_TIME=5 # seconds
+MAX_TIME=5 # seconds
 
 response=$(curl -m "$MAX_TIME" -sf -H "Accept: application/json" ipinfo.io/json)
 if [ -n "$response" ] && ! echo "$response" | jq -r '.ip' | grep -iq null; then
