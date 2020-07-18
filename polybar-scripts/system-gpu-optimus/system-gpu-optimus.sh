09957c70fe3fe513b69c0e01793d28aec6a1d588
x70b1 <x70b1@users.noreply.github.com>
2020-07-19 00:18:40 +0200

    system-gpu-optimus: simplify script

--- polybar-scripts/system-gpu-optimus/system-gpu-optimus.sh
+++ polybar-scripts/system-gpu-optimus/system-gpu-optimus.sh
@@ -1,85 +1,53 @@
 #!/bin/sh
 
-# Simple script for displaying and switching gpus for optimus-manager and polybar.
+icon_intel="#1"
+icon_nvidia="#2"
+icon_hybrid="#3"
 
-
-# Configuration
-
-# Set the symbols to be displayed in polybar
-intel_symbol="GPU: Intel"
-nvidia_symbol="GPU: Nvidia"
-hybrid_symbol="GPU: Hybrid"
-
-# Prefered setup. 
-# Set to 1 if the system switches between hybrid and nvidia.
-# Set to 0 if the system switches between intel and nvidia.
 hybrid_switching=0
 
 
-# Functions
-
-contains() {
-    string="$1"
-    substring="$2"
-    if test "${string#*$substring}" != "$string"
-    then
-        return 0
-    else
-        return 1
-    fi
-}
-
-get_current(){
+gpu_current() {
 	mode=$(optimus-manager --print-mode)
-    mode_trimmed="$(echo  "${mode}" | tr -d '[:space:]')"
 
-    arg1=intel;
-    arg2=nvidia;
-    arg3=hybrid;
-	
-    contains "$mode_trimmed" "$arg1" && echo "$arg1" 
-	contains "$mode_trimmed" "$arg2" && echo "$arg2"
-	contains "$mode_trimmed" "$arg3" && echo "$arg3"
+    echo "$mode" | cut -d ' ' -f 4
 }
 
-display_gpu(){
+gpu_switch() {
+    mode=$(gpu_current)
 
-
-    if [ "$(get_current)" = "intel" ]; then
-		echo "$intel_symbol"
-	elif [ "$(get_current)" = "nvidia" ]; then
-		echo "$nvidia_symbol"
-	elif [ "$(get_current)" = "hybrid" ]; then
-		echo "$hybrid_symbol"
-	fi
-}
-
-switch_gpu(){
-	if [ "$(get_current)" = "intel" ]; then
+	if [ "$mode" = "intel" ]; then
 		next="nvidia"
-	elif [ "$(get_current)" = "nvidia" ]; then
+	elif [ "$mode" = "nvidia" ]; then
 		if [ "$hybrid_switching" = 1 ]; then
 			next="hybrid"
 		else
 			next="intel"
 		fi
-	elif [ "$(get_current)" = "hybrid" ]; then
+	elif [ "$mode" = "hybrid" ]; then
 		next="nvidia"
-	fi	
+	fi
+
 	optimus-manager --switch $next --no-confirm
 }
 
-# Handle arguments. 
+gpu_display(){
+    mode=$(gpu_current)
+
+    if [ "$mode" = "intel" ]; then
+		echo "$icon_intel"
+	elif [ "$mode" = "nvidia" ]; then
+		echo "$icon_nvidia"
+	elif [ "$mode" = "hybrid" ]; then
+		echo "$icon_hybrid"
+	fi
+}
 
 case "$1" in
 	--switch)
-	switch_gpu
-	;;
-*)
-	display_gpu
-	;;
+        gpu_switch
+        ;;
+    *)
+        gpu_display
+        ;;
 esac
-
-
-	
-
