b60561e86459fe67e4d976162341e5ba116b29a3
Andreas Loholt <andreas.loholt@evry.com>
2020-08-01 03:23:34 +0200

    SC2046

--- polybar-scripts/info-active-window/window.sh
+++ polybar-scripts/info-active-window/window.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! wm_class=$(xprop -id $(xdotool getactivewindow) WM_CLASS | awk '{print $4}' | sed 's/"//g' | sed 's/-/ /g'); then
+if ! wm_class=$(xprop -id "$(xdotool getactivewindow)" WM_CLASS | awk '{print $4}' | sed 's/"//g' | sed 's/-/ /g'); then
     :
 else
     echo "$wm_class"
