2bac626be99dd1f6f92adbc447edaee5dafa7b06
pryme-svg <edoc.www@gmail.com>
2020-12-20 18:26:17 -0800

    add stopwatch

---
+++ polybar-scripts/utiliy-stopwatch/README.md
@@ -0,0 +1,18 @@
+# Script: utility-stopwatch
+
+A simple stopwatch script
+
+![utility-stopwatch](screenshots/1.png) 
+
+## Module 
+
+```ini
+[module/stopwatch]
+type = custom/script
+
+exec = ~/polybar-scripts/polybar-stopwatch.sh display
+tail = true
+
+click-left = ~/polybar-scripts/polybar-stopwatch.sh new
+click-right = ~/polybar-scripts/polybar-stopwatch.sh cancel ; ~/polybar-scripts/polybar-stopwatch.sh display
+```
---
+++ polybar-scripts/utiliy-stopwatch/stopwatch.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+pid=$$
+
+
+startTimer () {
+	sec=0
+	startdate=$(date +%s)
+	while true
+	do 
+		let "sec = $(date +%s) - $startdate + 1"
+
+		if [ "$sec" -lt 60 ]
+			# fill time with raw seconds
+			then time="$sec"
+		# switch to minutes if more than 60 seconds
+		else min=$(expr $sec / 60)
+			# calculate the seconds minus the minutes
+			relSec=$(expr $sec - $min \* 60)
+			# fill time with minutes and relative seconds
+			time="$min"'m'" $relSec"
+		fi
+
+		echo "祥 $time" >> /tmp/polybar-stopwatch
+		sleep 1
+	done
+}
+
+initialize () {
+	echo 祥 > /tmp/polybar-stopwatch 
+}
+
+initialize 
+
+case $1 in
+  new)
+    echo $pid > /tmp/polybar-stopwatch-pid
+    startTimer
+    ;;
+display)
+    echo 祥 >> /tmp/polybar-stopwatch 
+    ;;
+tail)
+    { tail -f /tmp/polybar-stopwatch 2>&1 >&3 3>&- | grep -v truncated >&2 3>&-;} 3>&1
+    ;;
+  cancel)
+    kill $(cat /tmp/polybar-stopwatch-pid)
+    ;;
+  *)
+    echo "Invalid Option"
+    ;;
+esac
