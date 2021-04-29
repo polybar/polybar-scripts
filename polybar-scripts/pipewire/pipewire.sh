6f2672e5de7da4fd15734d84dbea3d26dbee047f
Victor Trac <victor@cloudkite.io>
2021-04-28 22:06:06 -0400

    Improve description robustness

--- polybar-scripts/pipewire/pipewire.sh
+++ polybar-scripts/pipewire/pipewire.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 function main() {
-    SOURCE=$(pw-record --list-targets | sed -n 's/^*.*"\(.*\)" prio=.*$/\1/p')
-    SINK=$(pw-play --list-targets | sed -n 's/^*.*"\(.*\)" prio=.*$/\1/p')
-    VOLUME=$(pactl list sinks | sed -n "/${SINK}/,/Volume/ s!^[[:space:]]\+Volume:.* \([[:digit:]]\+\)%.*!\1!p")
-    IS_MUTED=$(pactl list sinks | sed -n "/${SINK}/,/Mute/ s/Mute: \(yes\)/\1/p")
+    DEFAULT_SOURCE=$(pw-record --list-targets | sed -n 's/^*[[:space:]]*[[:digit:]]\+: description="\(.*\)" prio=[[:digit:]]\+$/\1/p')
+    DEFAULT_SINK_ID=$(pw-play --list-targets | sed -n 's/^*[[:space:]]*\([[:digit:]]\+\):.*$/\1/p')
+    DEFAULT_SINK=$(pw-play --list-targets | sed -n 's/^*[[:space:]]*[[:digit:]]\+: description="\(.*\)" prio=[[:digit:]]\+$/\1/p')
+    VOLUME=$(pactl list sinks | sed -n "/Sink #${DEFAULT_SINK_ID}/,/Volume/ s!^[[:space:]]\+Volume:.* \([[:digit:]]\+\)%.*!\1!p" | head -n1)
+    IS_MUTED=$(pactl list sinks | sed -n "/Sink #${DEFAULT_SINK_ID}/,/Mute/ s/Mute: \(yes\)/\1/p")
 
     action=$1
     if [ "${action}" == "up" ]; then
@@ -15,9 +16,9 @@ function main() {
         pactl set-sink-mute @DEFAULT_SINK@ toggle
     else
         if [ "${IS_MUTED}" != "" ]; then
-            echo " ${SOURCE} |   MUTED ${SINK}"
+            echo " ${DEFAULT_SOURCE} |   MUTED ${DEFAULT_SINK}"
         else
-            echo " ${SOURCE} |    ${VOLUME}% ${SINK}"
+            echo " ${DEFAULT_SOURCE} |    ${VOLUME}% ${DEFAULT_SINK}"
         fi
     fi
 }
