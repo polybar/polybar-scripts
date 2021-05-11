996019b2eeb6026e7160ffb11f02194d04979754
RealStickman <61015582+RealStickman@users.noreply.github.com>
2021-05-11 15:16:41 +0200

    updates-aurhelper,updates-pacman-aurhelper: add paru example

--- polybar-scripts/updates-pacman-aurhelper/updates-pacman-aurhelper.sh
+++ polybar-scripts/updates-pacman-aurhelper/updates-pacman-aurhelper.sh
@@ -5,6 +5,7 @@ if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
 fi
 
 if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+# if ! updates_aur=$(paru -Qum 2> /dev/null | wc -l); then
 # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
 # if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
 # if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
