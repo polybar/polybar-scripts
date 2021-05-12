0fc363b18676d8e254189b08ff1645cec5e20644
x70b1 <10016267+x70b1@users.noreply.github.com>
2021-05-12 23:36:53 +0200

    keep icons as #1, #2 ..

--- polybar-scripts/system-gpu-optimus/system-gpu-optimus.sh
+++ polybar-scripts/system-gpu-optimus/system-gpu-optimus.sh
@@ -1,53 +1,53 @@
 #!/bin/sh
 
-icon_intel="Intel"
-icon_nvidia="Nvidia"
-icon_hybrid="Hybrid"
+icon_intel="#1"
+icon_nvidia="#2"
+icon_hybrid="#3"
 
-hybrid_switching=1
+hybrid_switching=0
+
 
 gpu_current() {
-  mode=$(optimus-manager --print-mode)
-  echo "$mode" | cut -d ' ' -f 5 | tr -d '[:space:]'
+	mode=$(optimus-manager --print-mode)
+
+    echo "$mode" | cut -d ' ' -f 5 | tr -d '[:space:]'
 }
 
 gpu_switch() {
-  mode=$(gpu_current)
+    mode=$(gpu_current)
 
-  if [ "$mode" = "intel" ]; then
-    next="nvidia"
-  elif [ "$mode" = "nvidia" ]; then
-    if [ "$hybrid_switching" = 1 ]; then
-      next="hybrid"
-    else
-      next="intel"
-    fi
-  elif [ "$mode" = "hybrid" ]; then
-    next="nvidia"
-  fi
+	if [ "$mode" = "intel" ]; then
+		next="nvidia"
+	elif [ "$mode" = "nvidia" ]; then
+		if [ "$hybrid_switching" = 1 ]; then
+			next="hybrid"
+		else
+			next="intel"
+		fi
+	elif [ "$mode" = "hybrid" ]; then
+		next="nvidia"
+	fi
 
-  optimus-manager --switch $next --no-confirm
+	optimus-manager --switch $next --no-confirm
 }
 
 gpu_display(){
-  mode=$(gpu_current)
+    mode=$(gpu_current)
 
-  if [ "$mode" = "intel" ]; then
-    echo "$icon_intel"
-  elif [ "$mode" = "nvidia" ]; then
-    echo "$icon_nvidia"
-  elif [ "$mode" = "hybrid" ]; then
-    echo "$icon_hybrid"
-  else
-    echo "Unknown"
-  fi
+    if [ "$mode" = "intel" ]; then
+		echo "$icon_intel"
+	elif [ "$mode" = "nvidia" ]; then
+		echo "$icon_nvidia"
+	elif [ "$mode" = "hybrid" ]; then
+		echo "$icon_hybrid"
+	fi
 }
 
 case "$1" in
-  --switch)
-    gpu_switch
-    ;;
-  *)
-    gpu_display
-    ;;
+	--switch)
+        gpu_switch
+        ;;
+    *)
+        gpu_display
+        ;;
 esac
