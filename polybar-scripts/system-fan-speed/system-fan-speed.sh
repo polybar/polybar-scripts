91ac35f1ca3758cfca5e28b606d0a3bee91c787a
x70b1 <10016267+x70b1@users.noreply.github.com>
2021-05-14 00:14:23 +0200

    system-fan-speed: use printf instead of scale

--- polybar-scripts/system-fan-speed/system-fan-speed.sh
+++ polybar-scripts/system-fan-speed/system-fan-speed.sh
@@ -3,7 +3,7 @@
 speed=$(sensors | grep fan1 | awk '{print $2; exit}')
 
 if [ "$speed" != "" ]; then
-    speed_round=$(echo "scale=1;$speed/1000" | bc -l )
+    speed_round=$(echo "$speed/1000" | bc -l | LANG=C xargs printf "%.1f\n")
     echo "# $speed_round"
 else
    echo "#"
