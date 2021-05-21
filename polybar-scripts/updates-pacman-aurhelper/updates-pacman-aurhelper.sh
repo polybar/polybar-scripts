263e55b83f8414c9b136fa9498c495fc99e986c5
druskus20 <druskus20@gmail.com>
2021-05-21 23:22:34 +0200

    Reverted shebang for updates-pacman-aurhelper, fixed quotes instead

--- polybar-scripts/updates-pacman-aurhelper/updates-pacman-aurhelper.sh
+++ polybar-scripts/updates-pacman-aurhelper/updates-pacman-aurhelper.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
@@ -13,7 +13,7 @@ if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
     updates_aur=0
 fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
+updates=$((updates_arch + updates_aur))
 
 if [ "$updates" -gt 0 ]; then
     echo "# $updates"
