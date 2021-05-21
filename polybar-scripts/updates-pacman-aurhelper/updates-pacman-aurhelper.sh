07a137348de908d6c2342306d2abaf5a7ad9f478
Pedro Burgos <43417195+druskus20@users.noreply.github.com>
2021-05-21 23:31:51 +0200

    updates-pacman-aurhelper: fix quotes

--- polybar-scripts/updates-pacman-aurhelper/updates-pacman-aurhelper.sh
+++ polybar-scripts/updates-pacman-aurhelper/updates-pacman-aurhelper.sh
@@ -13,7 +13,7 @@ if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
     updates_aur=0
 fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
+updates=$((updates_arch + updates_aur))
 
 if [ "$updates" -gt 0 ]; then
     echo "# $updates"
