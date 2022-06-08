f7faf0d8b5bd5776d3ae499dc13592af2472eb6b
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-06-08 19:57:58 +0200

    added github icon (nerd fonts)
    added github icon (nerd fonts)

--- polybar-scripts/notification-github/notification-github.sh
+++ polybar-scripts/notification-github/notification-github.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-USER=""
+USER="yourUserName"
+
+# You can get your Personal access tokens from here : https://github.com/settings/tokens #
 TOKEN="1234567890"
 
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
 if [ "$notifications" -gt 0 ]; then
-    echo "# $notifications"
+    echo " $notifications"
 else
-    echo ""
+    echo " 0"
 fi
