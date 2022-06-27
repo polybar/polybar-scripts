82c63ab5fe536a617e928ee51001f77ce21bca5c
tactonbishop <tactonbishop@gmail.com>
2022-06-27 20:10:06 +0100

    Fix error, add error handling

--- polybar-scripts/info-wakatime/info-wakatime.sh
+++ polybar-scripts/info-wakatime/info-wakatime.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
 TOKEN=""
 
-curl -s --header "Authorization: Basic $(echo "$TOKEN" | base64)" https://wakatime.com/api/v1/users/current/status_bar/today  | jq -r '.data.grand_total.text'
+wakatime_today="$(curl -sf --header "Authorization: Basic $(echo "$TOKEN" | base64)" https://wakatime.com/api/v1/users/current/status_bar/today | jq -r '.data.grand_total.text')"
+
+if [ -n "$wakatime_today" ]; then
+    echo "#1 $wakatime_today"
+else
+    echo "#2"
+fi
