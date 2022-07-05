6d6ea6a25675a6f840ac300598cea6abea747ae1
Victor Trac <victor@cloudkite.io>
2022-07-05 10:32:58 -0500

    Simplify and fix pipewire-simple regex

--- polybar-scripts/pipewire-simple/pipewire-simple.sh
+++ polybar-scripts/pipewire-simple/pipewire-simple.sh
@@ -2,13 +2,13 @@
 
 getDefaultSink() {
     defaultSink=$(pactl info | awk -F : '/Default Sink:/{print $2}')
-    description=$(pactl list sinks | sed -n "/${defaultSink}/,/Description/p; /Description/q" | sed -n 's/^.*Description: \(.*\)$/\1/p')
+    description=$(pactl list sinks | sed -n "/${defaultSink}/,/Description/s/^\s*Description: \(.*\)/\1/p")
     echo "${description}"
 }
 
 getDefaultSource() {
     defaultSource=$(pactl info | awk -F : '/Default Source:/{print $2}')
-    description=$(pactl list sources | sed -n "/${defaultSource}/,/Description/p; /Description/q" | sed -n 's/^.*Description: \(.*\)$/\1/p')
+    description=$(pactl list sources | sed -n "/${defaultSource}/,/Description/s/^\s*Description: \(.*\)/\1/p")
     echo "${description}"
 }
 
