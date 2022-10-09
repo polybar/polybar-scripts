1dfd00f719b9daf0e9b0553a9a02e29a50a0fed7
John Molakvoæ <skjnldsv@protonmail.com>
2022-10-09 19:18:21 +0200

    Fix github notifications and paginate properly
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- polybar-scripts/notification-github/README.md
+++ polybar-scripts/notification-github/README.md
@@ -12,6 +12,7 @@ A small script that shows your GitHub notifications.
 ## Configuration
 
 Generate a token at `GitHub Settings` > `Developer settings` > `Personal access tokens`.
+Make sure you have it saved as an env variable named GITHUB_TOKEN.
 
 
 ## Module
@@ -20,5 +21,8 @@ Generate a token at `GitHub Settings` > `Developer settings` > `Personal access 
 [module/notification-github]
 type = custom/script
 exec = ~/polybar-scripts/notification-github.sh
+# Adjust interval based on github rate limit
+# https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
 interval = 60
+click-left = xdg-open 'https://github.com/notifications' &
 ```
--- polybar-scripts/notification-github/notification-github.sh
+++ polybar-scripts/notification-github/notification-github.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 
-USER=""
-TOKEN="1234567890"
+PAGE=1
+PER_PAGE=50
+NOTIFICATIONS=0
 
-notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
+# Loop all pages
+until [[ $(( $NOTIFICATIONS % $PER_PAGE )) != 0 ]]
+do
+    response=$(curl --silent -H "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/notifications?per_page=${PER_PAGE}&page=${PAGE}" | jq -c)
+    has_message=$(echo $response | jq -r '.message?')
 
-if [ "$notifications" -gt 0 ]; then
-    echo "# $notifications"
-else
-    echo ""
-fi
+    # If github returns an error, display it.
+    # e.g "Bad credentials"
+    if [[ ! -z "$has_message" ]]; then
+        echo $has_message
+        exit 0
+    fi
+
+    # If the curl is empty, the network is most likely down.
+    if [[ ! "$response" ]]; then
+        echo "Github unreachable"
+        exit 0
+    fi
+
+    # Count all notifications
+    count=$(echo $response | jq '.[].unread' | grep -c true)
+    NOTIFICATIONS=$(($count + $NOTIFICATIONS))
+    PAGE=$(($PAGE + 1))
+done
+
+echo $NOTIFICATIONS
