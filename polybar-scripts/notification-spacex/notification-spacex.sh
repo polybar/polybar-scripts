f4737b7ebdd63cc6e6c4f10deb37b27f3260832e
krzys-h <krzys_h@interia.pl>
2022-10-14 14:33:19 +0200

    notification-spacex: Upgrade to v5 API
    The v3 API no longer recieves new data since November 2020

--- polybar-scripts/notification-spacex/notification-spacex.sh
+++ polybar-scripts/notification-spacex/notification-spacex.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-spacex_launch=$(curl -sf https://api.spacexdata.com/v3/launches/next)
+spacex_launch=$(curl -sf https://api.spacexdata.com/v5/launches/next)
 
 if [ -n "$spacex_launch" ]; then
-    spacex_precision=$(echo "$spacex_launch" | jq -r '.tentative_max_precision' )
-    spacex_timestamp=$(echo "$spacex_launch" | jq -r '.launch_date_unix' )
+    spacex_precision=$(echo "$spacex_launch" | jq -r '.date_precision' )
+    spacex_timestamp=$(echo "$spacex_launch" | jq -r '.date_unix' )
     spacex_duration=$((spacex_timestamp - $(date +%s)))
 
-    if [ "$spacex_precision" = "hour" ] && [ "$spacex_duration" -lt 43200 ]; then
+    if [ "$spacex_precision" = "hour" ] && [ "$spacex_duration" -lt 43200 ] && [ "$spacex_duration" -gt 0 ]; then
         echo "# $(date +"%H:%M" -u --date @$spacex_duration)"
     else
         echo ""
