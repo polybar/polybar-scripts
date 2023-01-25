fcd1ad1d49227612afcca0b5cd8fa89bb03b8c62
2trvl <38960745+2trvl@users.noreply.github.com>
2023-01-25 22:23:53 +0300

    better bluetooth

--- polybar-scripts/system-bluetooth-bluetoothctl/README.md
+++ polybar-scripts/system-bluetooth-bluetoothctl/README.md
@@ -1,28 +1,51 @@
 # Script: system-bluetooth-bluetoothctl
 
-A shell script which displays the status of bluetooth and the paired devices.
+A shell script which displays information about the current bluetooth connection.
 
 Use the toggle option to power on the controller and try to connect to all paired devices or to disconnect all connections and turn off the controller.
 
-![system-bluetooth-bluetoothctl](screenshots/1.png)
+If there are several connections, then one is selected in the following order:
+1) sending file, newest obexftp process
+2) audio headset
+3) device, first in the list
+
+![bluetooth-statuses](screenshots/statuses.png)
 
 
 ## Dependencies
 
 * `bluetoothctl`
+* `bluez-deprecated, obexftp (optional)`
 
 
 ## Configuration
 
 Use the `set-alias` feature of `bluetoothctl` to customize your device names.
 
+To send file with obexftp use:
+```Shell
+# check with bluetoothctl devices Connected
+MAC_ADDR=<your device mac address>
+CHANNEL=$(sdptool search --bdaddr $MAC_ADDR OPUSH | awk '/Channel/ {printf $2}')
+obexftp -S -H -U none -b $MAC_ADDR -B $CHANNEL -p /path/to/send
+```
+
 
 ## Module
 
 ```ini
-[module/system-bluetooth-bluetoothctl]
+[module/bluetooth]
 type = custom/script
 exec = ~/polybar-scripts/system-bluetooth-bluetoothctl.sh
 tail = true
+interval = 2
 click-left = ~/polybar-scripts/system-bluetooth-bluetoothctl.sh --toggle &
+click-right = blueman-manager &
+...
+```
+
+You can also change the icon font with --format by specifying your font number.
+For example, to use font-1:
+```ini
+exec = ~/polybar-scripts/system-bluetooth-bluetoothctl.sh --format 2
 ```
--- polybar-scripts/system-bluetooth-bluetoothctl/system-bluetooth-bluetoothctl.sh
+++ polybar-scripts/system-bluetooth-bluetoothctl/system-bluetooth-bluetoothctl.sh
@@ -1,60 +1,73 @@
 #!/bin/sh
 
+_status=$(bluetoothctl show | awk '/Powered/ {printf $2}')
+
 bluetooth_print() {
-    bluetoothctl | while read -r; do
-        if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
-            printf '#1'
-
-            devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
-            counter=0
-
-            for device in $devices_paired; do
-                device_info=$(bluetoothctl info "$device")
-
-                if echo "$device_info" | grep -q "Connected: yes"; then
-                    device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
-
-                    if [ $counter -gt 0 ]; then
-                        printf ", %s" "$device_alias"
-                    else
-                        printf " %s" "$device_alias"
-                    fi
-
-                    counter=$((counter + 1))
-                fi
-            done
-
-            printf '\n'
+    if [ "$_status" = "yes" ]; then
+        _icon=''
+        _label="$_icon"
+        
+        _obex_pid=$(pgrep -f obexftp -n)
+        
+        if [ -n "$_obex_pid" ]; then
+            _devices=( $(ps -p $_obex_pid -o args --no-headers | awk 'match ($0, /([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})/) {printf substr($0, RSTART, RLENGTH)}') )
         else
-            echo "#2"
+            _devices=( $(bluetoothctl devices Connected | awk '{print $2}') )
         fi
-    done
+        
+        for _device in "${_devices[@]}"; do
+            _info=$(bluetoothctl info $_device)
+            _type=$(echo "$_info" | awk '/Icon/ {printf $2}')
+            _name=$(echo "$_info" | awk '/Alias/ {for (i=2; i<NF; i++) printf $i " "; print $NF}')
+
+            case $_type in
+                audio*)
+                    _icon=''
+                    _battery=$(echo "$_info" | awk '/Battery/ {gsub(/[()]/,"",$4); printf $4}')
+                    _label="$_icon $_name, $_battery%"
+                    break
+                    ;;
+                *)
+                    if [ -n "$_obex_pid" ]; then
+                        _icon=''
+                        _label="$_icon $_name"
+                    elif [ -z "${_label:1}" ]; then
+                        _icon=''
+                        _label="$_icon $_name"
+                    fi
+                    ;;
+            esac
+        done
+        echo "$_label"
+    else
+        echo ""
+    fi
+}
+
+bluetooth_format() {
+    _output=$(bluetooth_print)
+    echo "%{T$1}${_output:0:2}%{T-}${_output:2}"
 }
 
 bluetooth_toggle() {
-    if bluetoothctl show | grep -q "Powered: no"; then
-        bluetoothctl power on >> /dev/null
-        sleep 1
-
-        devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
-        echo "$devices_paired" | while read -r line; do
-            bluetoothctl connect "$line" >> /dev/null
+    if [ "$_status" = "yes" ]; then
+        _devices=( $(bluetoothctl devices Connected | awk '{print $2}') )
+        for _device in "${_devices[@]}"; do
+            bluetoothctl disconnect $_device > /dev/null
         done
+        bluetoothctl power off > /dev/null
     else
-        devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
-        echo "$devices_paired" | while read -r line; do
-            bluetoothctl disconnect "$line" >> /dev/null
+        bluetoothctl power on > /dev/null
+        sleep 1
+        _devices=( $(bluetoothctl devices Paired | awk '{print $2}') )
+        for _device in "${_devices[@]}"; do
+            bluetoothctl connect $_device > /dev/null
         done
-
-        bluetoothctl power off >> /dev/null
     fi
 }
 
 case "$1" in
-    --toggle)
-        bluetooth_toggle
-        ;;
-    *)
-        bluetooth_print
-        ;;
+    --format)    bluetooth_format $2 ;;
+    --toggle)    bluetooth_toggle ;;
+    *)           bluetooth_print ;;
 esac
