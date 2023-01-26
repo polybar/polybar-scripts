bbb191275c830e742e1e46c11ee31c2225f42a56
2trvl <38960745+2trvl@users.noreply.github.com>
2023-01-26 22:58:42 +0300

    port to posix sh

--- polybar-scripts/system-bluetooth-bluetoothctl/system-bluetooth-bluetoothctl.sh
+++ polybar-scripts/system-bluetooth-bluetoothctl/system-bluetooth-bluetoothctl.sh
@@ -10,13 +10,13 @@ bluetooth_print() {
         _obex_pid=$(pgrep -f obexftp -n)
         
         if [ -n "$_obex_pid" ]; then
-            _devices=( $(ps -p $_obex_pid -o args --no-headers | awk 'match ($0, /([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})/) {printf substr($0, RSTART, RLENGTH)}') )
+            set -- $(ps -p "$_obex_pid" -o args --no-headers | awk 'match ($0, /([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})/) {printf substr($0, RSTART, RLENGTH)}')
         else
-            _devices=( $(bluetoothctl devices Connected | awk '{print $2}') )
+            set -- $(bluetoothctl devices Connected | awk '{print $2}')
         fi
         
-        for _device in "${_devices[@]}"; do
-            _info=$(bluetoothctl info $_device)
+        for _device in "$@"; do
+            _info=$(bluetoothctl info "$_device")
             _type=$(echo "$_info" | awk '/Icon/ {printf $2}')
             _name=$(echo "$_info" | awk '/Alias/ {for (i=2; i<NF; i++) printf $i " "; print $NF}')
             
@@ -35,7 +35,7 @@ bluetooth_print() {
                     if [ -n "$_obex_pid" ]; then
                         _icon=''
                         _label="$_icon $_name"
-                    elif [ -z "${_label:1}" ]; then
+                    elif [ -z "$(echo "$_label" | cut -b 4-)" ]; then
                         _icon=''
                         _label="$_icon $_name"
                     fi
@@ -50,28 +50,28 @@ bluetooth_print() {
 
 bluetooth_format() {
     _output=$(bluetooth_print)
-    echo "%{T$1}${_output:0:2}%{T-}${_output:2}"
+    echo "%{T$1}$(echo "$_output" | cut -b -4)%{T-}$(echo "$_output" | cut -b 5-)"
 }
 
 bluetooth_toggle() {
     if [ "$_status" = "yes" ]; then
-        _devices=( $(bluetoothctl devices Connected | awk '{print $2}') )
-        for _device in "${_devices[@]}"; do
-            bluetoothctl disconnect $_device > /dev/null
+        set -- $(bluetoothctl devices Connected | awk '{print $2}')
+        for _device in "$@"; do
+            bluetoothctl disconnect "$_device" > /dev/null
         done
         bluetoothctl power off > /dev/null
     else
         bluetoothctl power on > /dev/null
         sleep 1
-        _devices=( $(bluetoothctl devices Paired | awk '{print $2}') )
-        for _device in "${_devices[@]}"; do
-            bluetoothctl connect $_device > /dev/null
+        set -- $(bluetoothctl devices Paired | awk '{print $2}')
+        for _device in "$@"; do
+            bluetoothctl connect "$_device" > /dev/null
         done
     fi
 }
 
 case "$1" in
-    --format)    bluetooth_format $2 ;;
+    --format)    bluetooth_format "$2" ;;
     --toggle)    bluetooth_toggle ;;
     *)           bluetooth_print ;;
 esac
