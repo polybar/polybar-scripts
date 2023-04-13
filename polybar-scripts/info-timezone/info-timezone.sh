bbcb23cd98c413c8848e9859229f5b98edff8b22
Ashwin Rajesh <ashwinr2k2@gmail.com>
2023-04-13 23:30:53 +1000

    fix script to comply with shellcheck

--- polybar-scripts/info-timezone/info-timezone.sh
+++ polybar-scripts/info-timezone/info-timezone.sh
@@ -4,18 +4,20 @@
 FORMAT="%I:%M %p"
 
 # Add the timezones of your choice. see `timedatectl list-timezones`.
-TIMEZONES=("UTC-0" "Australia/Sydney" "Asia/Kolkata" "America/Chicago")
+set -- "UTC-0" "Australia/Sydney" "Asia/Kolkata" "America/Chicago"
 
-TIMEZONES_LENGTH=${#TIMEZONES[@]}
-current_idx=0
+TIMEZONES_LENGTH=$#
+current_idx=1
 
 print_date() {
-  current_timezone=${TIMEZONES[${current_idx}]}
   TZ=${current_timezone} date +"${FORMAT}" | echo "${current_timezone}: $(cat -)"
 }
 
 update_current_timezone() {
-  current_idx=$(expr $((current_idx+1)) % $TIMEZONES_LENGTH)
+  current_idx=$(($((current_idx+1)) % $(("$TIMEZONES_LENGTH"+1))))
+  if [ $current_idx -lt 1 ]; then
+    current_idx=1
+  fi
 }
 
 click() {
@@ -26,7 +28,8 @@ click() {
 trap "click" USR1
 
 while true; do
-  print_date
+  eval "current_timezone=\${$current_idx}"
+  print_date current_timezone
   sleep 5 &
   wait
 done
