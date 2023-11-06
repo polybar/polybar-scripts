684d6ea78e762e9263290b95a06c669347fe268b
allddd <117767298+allddd@users.noreply.github.com>
2023-11-06 19:50:29 +0100

    style

--- polybar-scripts/pipewire-microphone/pipewire-microphone.sh
+++ polybar-scripts/pipewire-microphone/pipewire-microphone.sh
@@ -1,41 +1,39 @@
 #!/bin/sh
 
-_get_mic_default() {
+get_mic_default() {
     pactl info | awk '/Default Source:/ {print $3}'
 }
 
-_is_mic_muted() {
-    pactl get-source-mute "$(_get_mic_default)" | awk '{print $2}'
+is_mic_muted() {
+    pactl get-source-mute "$(get_mic_default)" | awk '{print $2}'
 }
 
-_get_mic_status() {
-    if [ "$(_is_mic_muted)" = "yes" ]; then
+get_mic_status() {
+    if [ "$(is_mic_muted)" = "yes" ]; then
         printf "%s\n" "#1"
     else
         printf "%s\n" "#2"
     fi
 }
 
-_listen() {
-    _get_mic_status
+listen() {
+    get_mic_status
     LANG=EN; pactl subscribe | while read -r event; do
         if printf "%s\n" "${event}" | grep -qE '(source|server)'; then
-            _get_mic_status
+            get_mic_status
         fi
     done
 }
 
-_toggle() {
+toggle() {
     pactl set-source-mute @DEFAULT_SOURCE@ toggle
 }
 
 case "${1}" in
     --toggle)
-        _toggle
+        toggle
         ;;
     *)
-        _listen
+        listen
         ;;
 esac
-
-# vim: ts=4 sw=4 et:
