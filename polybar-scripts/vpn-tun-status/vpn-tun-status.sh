f8716d912a0d724a7a05977efa27f1a408f0bcef
x70b1 <x70b1@users.noreply.github.com>
2024-02-05 11:41:56 +0100

    rename script to vpn-tun-status

--- polybar-scripts/vpn-tun-status/vpn-tun-status.sh
+++ polybar-scripts/vpn-tun-status/vpn-tun-status.sh
@@ -7,7 +7,7 @@ FORMAT_DOWN='%{F#FF0000}'
 # Count the number of tun adapters, e.g. tun0, tun1, etc
 TUNTAPS=$(ip -j tuntap list | jq -r '.[].ifname' | grep -c '^tun' )
 
-if [ "$TUNTAPS" != "0" ]; then 
+if [ "$TUNTAPS" != "0" ]; then
     # This filters the JSON output from ip to show "name ip.ip.ip.ip" for each adapter
     MESSAGE_UP=$(\
         ip -4 -j addr show \
@@ -16,7 +16,7 @@ if [ "$TUNTAPS" != "0" ]; then
         | jq -r 'join(", ")'\
     )
     echo "$FORMAT_UP$MESSAGE_UP"
-else 
+else
     MESSAGE_DOWN='VPN down'
     echo "$FORMAT_DOWN$MESSAGE_DOWN"
 fi
