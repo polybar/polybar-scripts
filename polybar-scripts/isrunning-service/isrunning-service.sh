fad7c43817ed760f8d014e43df6a17a7745a2239
x70b1 <x70b1@users.noreply.github.com>
2024-03-26 08:47:25 +0100

    isrunning-service: fix shellcheck

--- polybar-scripts/isrunning-service/isrunning-service.sh
+++ polybar-scripts/isrunning-service/isrunning-service.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+# shellcheck disable=SC2034
 
 UNIT="docker.service"
 
-journalctl --follow -o cat --unit $UNIT | while read -r; do
-	if [ "$(systemctl is-active "$UNIT")" = "active" ]; then
+journalctl --follow -o cat --unit $UNIT | while read -r line; do
+    if [ "$(systemctl is-active "$UNIT")" = "active" ]; then
         echo "#1"
     else
         echo "#2"
