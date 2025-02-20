afd2600c2893a24ec6b31bfed6fd540562dd5039
Cretezy <Cretezy@users.noreply.github.com>
2025-02-20 17:49:50 -0500

    Updated for Pi-Hole 6
    For Pi-Hole 5 and lower, use https://github.com/polybar/polybar-scripts/blob/7d514cdfa2ab4d3a8246c87a140e06e3ccce4cbe/polybar-scripts/pihole-status/pihole-status.sh

--- polybar-scripts/pihole-status/pihole-status.sh
+++ polybar-scripts/pihole-status/pihole-status.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 
 ENDPOINT="http://pi.hole/admin/api.php" # Pi-Hole API endpoint. Change host
-API_TOKEN="" # Get from /admin/settings.php?tab=api
+API_TOKEN="" # Get from /admin/settings/api under "Expert" -> "Advanced Settings" -> "Configure app password"
+# NOTE: You may want to increase webserver.api.max_sessions (in /admin/settings/all)
 
 DISABLE_TIME="300" # In seconds. Set to 0 to disable Pi-Hole forever
 REFRESH_INTERVAL="10" # In seconds. Set to "infinity" to disable refreshing
 
-# Uses Nerd Fonts for icon and lemonbar tags for coloring
 ON_LABEL="󰷱"
 OFF_LABEL="%{F#f00}󰷱%{F-}"
 
 
+get_sid() {
+  if [ -n "$session_validity" ] && [ "$session_validity" -gt "$(date +%s)" ]; then
+    echo "$session_sid"
+    return
+  fi
+  
+  local response=$(curl -k -s -X POST "$ENDPOINT/auth" --data "{\"password\":\"$API_TOKEN\"}")
+  session_sid=$(echo "$response" | jq -r '.session.sid')
+  session_validity=$(($(date +%s) + $(echo "$response" | jq -r '.session.validity')))
+  echo "$session_sid"
+}
+
 get_status() {
-  curl -s "$ENDPOINT?status&auth=$API_TOKEN" | jq -r '.status'
+  curl -k -s -H "X-FTL-SID: $(get_sid)" "$ENDPOINT/dns/blocking" | jq -r '.blocking'
 }
 
 sleep_pid=0
@@ -22,12 +34,12 @@ toggle() {
 
     case "$status" in
         enabled)
-            curl -s "$ENDPOINT?disable=$DISABLE_TIME&auth=$API_TOKEN" > /dev/null
+            curl -k -s -H "X-FTL-SID: $(get_sid)" -X POST "$ENDPOINT/dns/blocking" --data "{\"blocking\":false,\"timer\":$DISABLE_TIME}" > /dev/null
             echo "$OFF_LABEL"
             sleep 0.1 # some time for write to finish before refetch
             ;;
         disabled)
-            curl -s "$ENDPOINT?enable&auth=$API_TOKEN" > /dev/null
+            curl -k -s -H "X-FTL-SID: $(get_sid)" -X POST "$ENDPOINT/dns/blocking" --data "{\"blocking\":true}" > /dev/null
             echo "$ON_LABEL"
             sleep 0.1 # some time for write to finish before refetch
             ;;
